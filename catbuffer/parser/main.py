7127a790978fca1f02ae111ddacac54038b99727
gimer <gimre@127.0.0.1>
2018-11-06 19:48:14 +0100

    Support multiple generated files per single cats file

--- catbuffer/parser/main.py
+++ catbuffer/parser/main.py
@@ -3,6 +3,7 @@ import argparse
 import os
 import pprint
 from catparser.CatsParser import CatsParser
+from generators.All import AVAILABLE_GENERATORS
 
 
 class MultiFileParser:
@@ -30,19 +31,39 @@ class MultiFileParser:
         self.cats_parser.pop_scope()
 
 
+def _generate_output(generator_name, directory, schema):
+    generator_class = AVAILABLE_GENERATORS[generator_name]
+    output_path = os.path.join(directory, generator_name)
+    os.makedirs(output_path, exist_ok=True)
+    generator = generator_class(schema)
+    for generated_descriptor in generator:
+        output_filename = os.path.join(output_path, generated_descriptor.filename)
+        with open(output_filename, 'w', newline='\n') as output_file:
+            for line in generated_descriptor.code:
+                output_file.write("%s\n" % line)
+
+
 def generate():
     parser = argparse.ArgumentParser(description='CATS code generator')
     parser.add_argument('-i', '--input', help='the input CATS file', required=True)
+    generators_list = list(AVAILABLE_GENERATORS.keys())
+    parser.add_argument('-d', '--dir', help='output directory', default='_generated')
+    parser.add_argument('-g', '--generator', help='the generator to use to produce output files', choices=generators_list)
     args = parser.parse_args()
 
     file_parser = MultiFileParser()
     file_parser.parse(args.input)
 
+    # console output the parsed schema
     printer = pprint.PrettyPrinter(width=140)
     printer.pprint('*** *** ***')
     type_descriptors = file_parser.cats_parser.type_descriptors()
     for key in type_descriptors:
         printer.pprint((key, type_descriptors[key]))
+
+    # generate and output code
+    if args.generator:
+        _generate_output(args.generator, args.dir, type_descriptors)
 
 
 generate()
