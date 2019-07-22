5dcd1da8471f82acf5cbf0606fce1977a41db18c
Jaguar0625 <jaguar0625@127.0.0.1>
2019-07-22 10:03:00 -0400

    update generation script to include elephant2 transaction types

--- catbuffer/parser/scripts/generate_all.sh
+++ catbuffer/parser/scripts/generate_all.sh
@@ -14,9 +14,13 @@ function generate_all {
 
 	local inputs=(
 		"account_link/account_link"
+		# "aggregate/aggregate" tracked by issue #26
 		"lock_hash/hash_lock"
 		"lock_secret/secret_lock"
 		"lock_secret/secret_proof"
+		"metadata/account_metadata"
+		"metadata/mosaic_metadata"
+		"metadata/namespace_metadata"
 		"mosaic/mosaic_definition"
 		"mosaic/mosaic_supply_change"
 		"multisig/modify_multisig_account"
@@ -26,6 +30,8 @@ function generate_all {
 		"restriction_account/account_address_restriction"
 		"restriction_account/account_mosaic_restriction"
 		"restriction_account/account_operation_restriction"
+		"restriction_mosaic/mosaic_address_restriction"
+		"restriction_mosaic/mosaic_global_restriction"
 		"transfer/transfer"
 	)
 
