c76b33be7383472b37586d011ca9e3474493abc7
Jaguar0625 <jaguar0625@127.0.0.1>
2019-08-02 14:36:56 -0400

    (scripts) fix shell array passing

--- catbuffer/parser/scripts/generate_batch.sh
+++ catbuffer/parser/scripts/generate_batch.sh
@@ -5,27 +5,32 @@ function generate_batch {
 	local start_error_color="\033[1;31m"
 	local end_color="\033[0m"
 
-	local inputs=$1
+	local -n inputs=$1
 	local folder="$2"
-	for input in ${inputs[*]}
+	local counter=0
+	for input in ${inputs[@]};
 	do
 		echo "generating ${input}"
-		python_args=(${folder}/main.py)
-		python_args+=(--schema ${folder}/schemas/${input}.cats)
-		python_args+=(--include ${folder}/schemas)
+		python_args=(
+			${folder}/main.py
+			--schema ${folder}/schemas/${input}.cats
+			--include ${folder}/schemas)
 		if [ "$#" -ge 3 ]; then
-			python_args+=(--output ${folder}/_generated)
-			python_args+=(--generator "$3")
-			python_args+=(--copyright "${folder}/HEADER.inc")
+			python_args+=(
+				--output ${folder}/_generated
+				--generator "$3"
+				--copyright "${folder}/HEADER.inc")
 		fi
 
 		python3 "${python_args[@]}"
 
 		if [ $? -ne 0 ]; then
-			echo "${start_error_color}ERROR: failed generating ${input}${end_color}"
+			echo -e "${start_error_color}ERROR: failed generating ${input}${end_color}"
 			exit 1
 		fi
+
+		((++counter))
 	done
 
-	echo "${start_success_color}SUCCESS: generation complete with no errors${end_color}"
+	echo -e "${start_success_color}SUCCESS: generation complete with no errors [${counter} files processed]${end_color}"
 }
