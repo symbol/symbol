d8219d049d90dd0b859fc42a999cb0c57de5d1b4
BloodyRookie <nemproject@gmx.de>
2020-01-29 16:05:17 +0100

    VRF implementation

---
+++ client/catapult/src/catapult/crypto/Vrf.cpp
@@ -0,0 +1,294 @@
+/**
+*** Copyright (c) 2016-present,
+*** Jaguar0625, gimre, BloodyRookie, Tech Bureau, Corp. All rights reserved.
+***
+*** This file is part of Catapult.
+***
+*** Catapult is free software: you can redistribute it and/or modify
+*** it under the terms of the GNU Lesser General Public License as published by
+*** the Free Software Foundation, either version 3 of the License, or
+*** (at your option) any later version.
+***
+*** Catapult is distributed in the hope that it will be useful,
+*** but WITHOUT ANY WARRANTY; without even the implied warranty of
+*** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+*** GNU Lesser General Public License for more details.
+***
+*** You should have received a copy of the GNU Lesser General Public License
+*** along with Catapult. If not, see <http://www.gnu.org/licenses/>.
+**/
+
+#include "Vrf.h"
+#include "CryptoUtils.h"
+#include "Hashes.h"
+#include "SecureZero.h"
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wold-style-cast"
+#pragma clang diagnostic ignored "-Wcast-align"
+#pragma clang diagnostic ignored "-Wcast-qual"
+#pragma clang diagnostic ignored "-Wimplicit-fallthrough"
+#pragma clang diagnostic ignored "-Wsign-conversion"
+#pragma clang diagnostic ignored "-Wunused-function"
+#pragma clang diagnostic ignored "-Wreserved-id-macro"
+#pragma clang diagnostic ignored "-Wdocumentation"
+#elif defined(__GNUC__)
+#pragma GCC diagnostic ignored "-Wunused-function"
+#pragma GCC diagnostic ignored "-Wstrict-aliasing"
+#pragma GCC diagnostic ignored "-Wimplicit-fallthrough"
+#elif defined(_MSC_VER)
+#pragma warning(push)
+#pragma warning(disable : 4324) /* ed25519 structs use __declspec(align()) */
+#pragma warning(disable : 4388) /* signed/unsigned mismatch */
+#pragma warning(disable : 4505) /* unreferenced local function has been removed */
+#endif
+
+extern "C" {
+#include <donna/ed25519-donna.h>
+#include <donna/modm-donna-64bit.h>
+}
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#elif defined(__GNUC__)
+#pragma GCC diagnostic pop
+#elif defined(_MSC_VER)
+#pragma warning(pop)
+#endif
+
+namespace catapult { namespace crypto {
+
+	namespace {
+		void SetZero(bignum25519 f) {
+			std::memset(&f[0], 0, 5 * sizeof(uint64_t));
+		}
+
+		void SetOne(bignum25519 f) {
+			f[0] = 1;
+			std::memset(&f[1], 0, 4 * sizeof(uint64_t));
+		}
+
+		void SetZero(ge25519& A) {
+			SetZero(A.x);
+			SetOne(A.y);
+			SetOne(A.z);
+			SetZero(A.t);
+		}
+
+		bool ge25519_unpack_positive_vartime(ge25519& A, const Key& publicKey) {
+			// unpack public key
+			if (!ge25519_unpack_negative_vartime(&A, publicKey.data()))
+				return false;
+
+			// negate A
+			ge25519_p1p1 B;
+			ge25519_pniels C;
+			ge25519 D;
+			ge25519_full_to_pniels(&C, &A);
+			SetZero(D);
+			ge25519_pnielsadd_p1p1(&B, &D, &C, 1);
+			ge25519_p1p1_to_full(&A, &B);
+
+			return true;
+		}
+
+		Key ScalarMultEight(const Key& key) {
+			ge25519 A;
+			if (!ge25519_unpack_positive_vartime(A, key))
+				return Key();
+
+			ge25519 B;
+			ge25519_double(&B, &A);
+			ge25519_double(&A, &B);
+			ge25519_double(&B, &A);
+
+			Key keyTimesEight;
+			ge25519_pack(keyTimesEight.data(), &B);
+			return keyTimesEight;
+		}
+
+		bool IsReduced(ScalarMultiplier& multiplier) {
+			ScalarMultiplier reduced;
+			bignum256modm temp;
+			expand256_modm(temp, multiplier, 32);
+			contract256_modm(reduced, temp);
+			return 0 == std::memcmp(reduced, multiplier, 32);
+		}
+
+		Hash512 IetfHash(uint8_t suite, uint8_t action, std::initializer_list<const RawBuffer> buffersList) {
+			Sha512_Builder builder;
+			builder.update({ &suite, 1 });
+			builder.update({ &action, 1 });
+			for (const auto& buffer : buffersList)
+				builder.update(buffer);
+
+			Hash512 hash;
+			builder.final(hash);
+			return hash;
+		}
+
+		Key MapToKey(const RawBuffer& alpha, const Key& publicKey) {
+			uint8_t i = 0;
+			ge25519 A;
+			while (true) {
+				// Hash(suite | action | publicKey | alpha | i)
+				auto hash = IetfHash(0x03, 0x01, { publicKey, alpha, { &i, 1 } });
+				if (ge25519_unpack_negative_vartime(&A, hash.data())) {
+					Key key;
+					std::memcpy(key.data(), hash.data(), key.size());
+					return ScalarMultEight(key);
+				}
+
+				++i;
+				if (0u == i)
+					return Key();
+			}
+		}
+
+		Key DoubleScalarMultVarTime(const ScalarMultiplier& encodedS, const Key& publicKey, const ScalarMultiplier& encodedC) {
+			ge25519 A;
+			if (!ge25519_unpack_negative_vartime(&A, publicKey.data()))
+				return Key();
+
+			bignum256modm S;
+			expand256_modm(S, encodedS, 32);
+
+			bignum256modm C;
+			expand256_modm(C, encodedC, 32);
+
+			ge25519 ALIGN(16) R;
+			ge25519_double_scalarmult_vartime(&R, &A, C, S);
+
+			Key packedR;
+			ge25519_pack(packedR.data(), &R);
+			return packedR;
+		}
+
+		ProofVerificationHash VrfC(const Key& h, const Key& gamma, const Key& k_times_B, const Key& k_times_h) {
+			auto hash = IetfHash(0x03, 0x02, { h, gamma, k_times_B, k_times_h });
+
+			ProofVerificationHash c;
+			std::memcpy(c.data(), hash.data(), 16);
+			return c;
+		}
+
+		ProofScalar VrfS(const ScalarMultiplier& encodedK, const ProofVerificationHash& c, const ScalarMultiplier& encodedX) {
+			bignum256modm C;
+			expand256_modm(C, c.data(), c.Size);
+
+			bignum256modm X;
+			expand256_modm(X, encodedX, sizeof(ScalarMultiplier));
+
+			bignum256modm CX;
+			mul256_modm(CX, C, X);
+
+			bignum256modm K;
+			expand256_modm(K, encodedK, sizeof(ScalarMultiplier));
+
+			bignum256modm S;
+			add256_modm(S, K, CX);
+
+			ProofScalar s;
+			contract256_modm(s.data(), S);
+			return s;
+		}
+	}
+
+	VrfProof GenerateVrfProof(const RawBuffer& alpha, const KeyPair& keyPair) {
+		// map to group element
+		auto h = MapToKey(alpha, keyPair.publicKey());
+		if (Key() == h)
+			return VrfProof();
+
+		// gamma = x * h
+		auto gamma = DeriveSharedSecret(keyPair, h);
+
+		// generate k
+		bignum256modm k;
+		GenerateNonce(keyPair.privateKey(), { h }, k);
+
+		// k * B
+		Key k_times_B;
+		ge25519 ALIGN(16) A;
+		ge25519_scalarmult_base_niels(&A, ge25519_niels_base_multiples, k);
+		ge25519_pack(k_times_B.data(), &A);
+
+		// k * h
+		Key k_times_h;
+		ScalarMultiplier encodedK;
+		contract256_modm(encodedK, k);
+		ScalarMult(encodedK, h, k_times_h);
+
+		// c = first 16 bytes of Sha512(suite | action | h | gamma | k * B | k * h)
+		auto c = VrfC(h, gamma, k_times_B, k_times_h);
+
+		// s = (k + cx) mod q
+		ScalarMultiplier encodedX;
+		ExtractMultiplier(keyPair.privateKey(), encodedX);
+		auto s = VrfS(encodedK, c, encodedX);
+
+		SecureZero(encodedK);
+		SecureZero(encodedX);
+
+		return { gamma, c, s };
+	}
+
+	Hash512 VerifyVrfProof(const VrfProof& vrfProof, const RawBuffer& alpha, const Key& publicKey) {
+		// gamma must be on the curve
+		ge25519 A;
+		if (!ge25519_unpack_negative_vartime(&A, vrfProof.Gamma.data()))
+			return Hash512();
+
+		// map to group element
+		auto h = MapToKey(alpha, publicKey);
+		if (Key() == h)
+			return Hash512();
+
+		// u = s * B - c * x_publicKey
+		ScalarMultiplier encodedS;
+		std::memcpy(encodedS, vrfProof.Scalar.data(), vrfProof.Scalar.size());
+		if (!IsReduced(encodedS))
+			return Hash512();
+
+		ScalarMultiplier encodedC{};
+		std::memcpy(encodedC, vrfProof.VerificationHash.data(), vrfProof.VerificationHash.size());
+		auto u = DoubleScalarMultVarTime(encodedS, publicKey, encodedC);
+
+		// V1 = s * h
+		// scalar multiplication cannot fail because s is small enough
+		Key v1;
+		ScalarMult(encodedS, h, v1);
+
+		// unpack cannot fail because V1 is valid by construction
+		ge25519 V1;
+		ge25519_unpack_positive_vartime(V1, v1);
+
+		// V2 = -(c * gamma)
+		// scalar multiplication cannot fail because c is small enough
+		Key c_times_gamma;
+		ScalarMult(encodedC, vrfProof.Gamma, c_times_gamma);
+
+		// unpack cannot fail because V2 is valid by construction
+		ge25519 V2_p3;
+		ge25519_unpack_negative_vartime(&V2_p3, c_times_gamma.data());
+
+		ge25519_pniels V2_pniels;
+		ge25519_full_to_pniels(&V2_pniels, &V2_p3);
+
+		// v = V1 + V2_pniels = s * h - c * gamma
+		ge25519 V;
+		ge25519_p1p1 B;
+		Key v;
+		ge25519_pnielsadd_p1p1(&B, &V1, &V2_pniels, 0);
+		ge25519_p1p1_to_full(&V, &B);
+		ge25519_pack(v.data(), &V);
+
+		// verificationHash = first 16 bytes of Sha512(suite | 0x2 | h | gamma | u | v)
+		auto verificationHash = VrfC(h, vrfProof.Gamma, u, v);
+
+		return 0 == std::memcmp(vrfProof.VerificationHash.data(), verificationHash.data(), verificationHash.size())
+				? IetfHash(0x03, 0x03, { ScalarMultEight(vrfProof.Gamma) })
+				: Hash512();
+	}
+}}
---
+++ client/catapult/tests/catapult/crypto/VrfTests.cpp
@@ -0,0 +1,177 @@
+/**
+*** Copyright (c) 2016-present,
+*** Jaguar0625, gimre, BloodyRookie, Tech Bureau, Corp. All rights reserved.
+***
+*** This file is part of Catapult.
+***
+*** Catapult is free software: you can redistribute it and/or modify
+*** it under the terms of the GNU Lesser General Public License as published by
+*** the Free Software Foundation, either version 3 of the License, or
+*** (at your option) any later version.
+***
+*** Catapult is distributed in the hope that it will be useful,
+*** but WITHOUT ANY WARRANTY; without even the implied warranty of
+*** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+*** GNU Lesser General Public License for more details.
+***
+*** You should have received a copy of the GNU Lesser General Public License
+*** along with Catapult. If not, see <http://www.gnu.org/licenses/>.
+**/
+
+#include "catapult/crypto/Vrf.h"
+#include "catapult/crypto/Hashes.h"
+#include "catapult/crypto/SharedKey.h"
+#include "catapult/utils/HexParser.h"
+#include "tests/test/crypto/CurveUtils.h"
+#include "tests/test/nodeps/KeyTestUtils.h"
+#include "tests/TestHarness.h"
+
+namespace catapult { namespace crypto {
+
+#define TEST_CLASS VrfTests
+
+	// region test vectors
+
+	namespace {
+		struct TestVectorsInput {
+			std::string SK;
+			std::string Alpha;
+		};
+
+		struct TestVectorOutput {
+			std::string Gamma;
+			std::string VerificationHash;
+			std::string Scalar;
+			std::string Beta;
+		};
+
+		// data taken from: https://www.ietf.org/id/draft-irtf-cfrg-vrf-05.txt
+		static std::vector<TestVectorsInput> SampleTestVectorsInput() {
+			return {
+				{ "9D61B19DEFFD5A60BA844AF492EC2CC44449C5697B326919703BAC031CAE7F60", ""},
+				{ "4CCD089B28FF96DA9DB6C346EC114E0F5B8A319F35ABA624DA8CF6ED4FB8A6FB", "72" },
+				{ "C5AA8DF43F9F837BEDB7442F31DCB7B166D38535076F094B85CE3A2E0B4458F7", "af82" }
+			};
+		}
+
+		static std::vector<TestVectorOutput> SampleTestVectorsOutput() {
+			return {
+				{
+					"9275DF67A68C8745C0FF97B48201EE6DB447F7C93B23AE24CDC2400F52FDB08A",
+					"1A6AC7EC71BF9C9C76E96EE4675EBFF6",
+					"0625AF28718501047BFD87B810C2D2139B73C23BD69DE66360953A642C2A330A",
+					"A64C292EC45F6B252828AFF9A02A0FE88D2FCC7F5FC61BB328F03F4C6C0657A9"
+					"D26EFB23B87647FF54F71CD51A6FA4C4E31661D8F72B41FF00AC4D2EEC2EA7B3"
+				},
+				{
+					"84A63E74ECA8FDD64E9972DCDA1C6F33D03CE3CD4D333FD6CC789DB12B5A7B9D",
+					"03F1CB6B2BF7CD81A2A20BACF6E1C04E",
+					"59F2FA16D9119C73A45A97194B504FB9A5C8CF37F6DA85E03368D6882E511008",
+					"CDDAA399BB9C56D3BE15792E43A6742FB72B1D248A7F24FD5CC585B232C26C93"
+					"4711393B4D97284B2BCCA588775B72DC0B0F4B5A195BC41F8D2B80B6981C784E"
+				},
+				{
+					"ACA8ADE9B7F03E2B149637629F95654C94FC9053C225EC21E5838F193AF2B727",
+					"B84AD849B0039AD38B41513FE5A66CDD",
+					"2367737A84B488D62486BD2FB110B4801A46BFCA770AF98E059158AC563B690F",
+					"D938B2012F2551B0E13A49568612EFFCBDCA2AED5D1D3A13F47E180E01218916"
+					"E049837BD246F66D5058E56D3413DBBBAD964F5E9F160A81C9A1355DCD99B453"
+				}
+			};
+		}
+	}
+
+	TEST(TEST_CLASS, VrfSampleTestVectors) {
+		// Arrange:
+		auto testVectorsInput = SampleTestVectorsInput();
+		auto testVectorsOutput = SampleTestVectorsOutput();
+
+		// Sanity:
+		ASSERT_EQ(testVectorsInput.size(), testVectorsOutput.size());
+
+		auto i = 0u;
+		for (const auto& input : testVectorsInput) {
+			// Arrange:
+			auto keyPair = KeyPair::FromString(input.SK);
+			auto alpha = test::HexStringToVector(input.Alpha);
+			auto message = "at index " + std::to_string(i);
+
+			// Act: compute proof
+			auto vrcProof = GenerateVrfProof(alpha, keyPair);
+
+			// Assert:
+			auto verificationHash = testVectorsOutput[i].VerificationHash;
+			EXPECT_EQ(utils::ParseByteArray<Key>(testVectorsOutput[i].Gamma), vrcProof.Gamma) << message;
+			EXPECT_EQ(utils::ParseByteArray<ProofVerificationHash>(verificationHash), vrcProof.VerificationHash) << message;
+			EXPECT_EQ(utils::ParseByteArray<ProofScalar>(testVectorsOutput[i].Scalar), vrcProof.Scalar) << message;
+
+			// Act: verify proof and compute beta
+			auto proofHash = VerifyVrfProof(vrcProof, alpha, keyPair.publicKey());
+
+			// Assert:
+			EXPECT_EQ(utils::ParseByteArray<Hash512>(testVectorsOutput[i].Beta), proofHash) << message;
+			++i;
+		}
+	}
+
+	// endregion
+
+	// region VerifyVrfProof
+
+	namespace {
+		void AssertVerifyVrfProofFailsWhenProofIsCorrupted(const consumer<VrfProof&>& transform) {
+			auto keyPair = KeyPair::FromString("9D61B19DEFFD5A60BA844AF492EC2CC44449C5697B326919703BAC031CAE7F60");
+			auto alpha = test::HexStringToVector("af82");
+			auto vrcProof = GenerateVrfProof(alpha, keyPair);
+
+			// Sanity:
+			auto proofHash = VerifyVrfProof(vrcProof, alpha, keyPair.publicKey());
+			EXPECT_NE(Hash512(), proofHash);
+
+			// Act: corrupt proof
+			transform(vrcProof);
+			proofHash = VerifyVrfProof(vrcProof, alpha, keyPair.publicKey());
+
+			// Assert:
+			EXPECT_EQ(Hash512(), proofHash);
+		}
+	}
+
+	TEST(TEST_CLASS, VerifyVrfProofFailsWhenGammaIsNotOnTheCurve) {
+		// Act: corrupt Gamma
+		AssertVerifyVrfProofFailsWhenProofIsCorrupted([](auto& vrfProof) {
+			vrfProof.Gamma = utils::ParseByteArray<Key>("4F91BE9568552181E01968999EFC09BFEB77A736B8F3188160B7769D7B9B9F6E");
+		});
+	}
+
+	TEST(TEST_CLASS, VerifyVrfProofFailsWhenGammaIsWrongPointOnCurve) {
+		// Act: corrupt Gamma
+		AssertVerifyVrfProofFailsWhenProofIsCorrupted([](auto& vrfProof) {
+			// valid public key
+			vrfProof.Gamma = utils::ParseByteArray<Key>("C8C6D604F4D7B56B57247E8686168EEBB2BF8AE40DA7B912143773A77555420E");
+		});
+	}
+
+	TEST(TEST_CLASS, VerifyVrfProofFailsWhenVerificationHashIsWrong) {
+		// Act: corrupt VerificationHash
+		AssertVerifyVrfProofFailsWhenProofIsCorrupted([](auto& vrfProof) {
+			vrfProof.VerificationHash[4] ^= 0xFF;
+		});
+	}
+
+	TEST(TEST_CLASS, VerifyVrfProofFailsWhenScalarIsWrong) {
+		// Act: corrupt Scalar
+		AssertVerifyVrfProofFailsWhenProofIsCorrupted([](auto& vrfProof) {
+			vrfProof.Scalar[14] ^= 0xFF;
+		});
+	}
+
+	TEST(TEST_CLASS, VerifyVrfProofFailsWhenScalarIsNotReduced) {
+		// Act: add group order to Scalar
+		AssertVerifyVrfProofFailsWhenProofIsCorrupted([](auto& vrfProof) {
+			test::ScalarAddGroupOrder(vrfProof.Scalar.data());
+		});
+	}
+
+	// endregion
+}}
