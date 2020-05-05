8ae1a82e3787219f6504d66e49d9193951c1ad62
Jaguar0625 <jaguar0625@127.0.0.1>
2020-05-05 14:13:23 -0400

    add NodeKeyLinkBuilder

---
+++ client/catapult/sdk/src/builders/NodeKeyLinkBuilder.cpp
@@ -0,0 +1,69 @@
+/**
+*** Copyright (c) 2016-present,
+*** Jaguar0625, gimre, BloodyRookie, Tech Bureau, Corp. All rights reserved.
+***
+*** This file is part of Catapult.
+***
+*** Catapult is free software: you can redistribute it and/or modify
+*** it under the terms of the GNU Lesser General Public License as published by
+*** the Free Software Foundation, either version 3 of the License, or
+*** (at your option) any later version.
+***
+*** Catapult is distributed in the hope that it will be useful,
+*** but WITHOUT ANY WARRANTY; without even the implied warranty of
+*** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+*** GNU Lesser General Public License for more details.
+***
+*** You should have received a copy of the GNU Lesser General Public License
+*** along with Catapult. If not, see <http://www.gnu.org/licenses/>.
+**/
+
+#include "NodeKeyLinkBuilder.h"
+
+namespace catapult { namespace builders {
+
+	NodeKeyLinkBuilder::NodeKeyLinkBuilder(model::NetworkIdentifier networkIdentifier, const Key& signer)
+			: TransactionBuilder(networkIdentifier, signer)
+			, m_linkedPublicKey()
+			, m_linkAction()
+	{}
+
+	void NodeKeyLinkBuilder::setLinkedPublicKey(const Key& linkedPublicKey) {
+		m_linkedPublicKey = linkedPublicKey;
+	}
+
+	void NodeKeyLinkBuilder::setLinkAction(model::LinkAction linkAction) {
+		m_linkAction = linkAction;
+	}
+
+	size_t NodeKeyLinkBuilder::size() const {
+		return sizeImpl<Transaction>();
+	}
+
+	std::unique_ptr<NodeKeyLinkBuilder::Transaction> NodeKeyLinkBuilder::build() const {
+		return buildImpl<Transaction>();
+	}
+
+	std::unique_ptr<NodeKeyLinkBuilder::EmbeddedTransaction> NodeKeyLinkBuilder::buildEmbedded() const {
+		return buildImpl<EmbeddedTransaction>();
+	}
+
+	template<typename TransactionType>
+	size_t NodeKeyLinkBuilder::sizeImpl() const {
+		// calculate transaction size
+		auto size = sizeof(TransactionType);
+		return size;
+	}
+
+	template<typename TransactionType>
+	std::unique_ptr<TransactionType> NodeKeyLinkBuilder::buildImpl() const {
+		// 1. allocate, zero (header), set model::Transaction fields
+		auto pTransaction = createTransaction<TransactionType>(sizeImpl<TransactionType>());
+
+		// 2. set fixed transaction fields
+		pTransaction->LinkedPublicKey = m_linkedPublicKey;
+		pTransaction->LinkAction = m_linkAction;
+
+		return pTransaction;
+	}
+}}
---
+++ client/catapult/sdk/src/builders/NodeKeyLinkBuilder.h
@@ -0,0 +1,67 @@
+/**
+*** Copyright (c) 2016-present,
+*** Jaguar0625, gimre, BloodyRookie, Tech Bureau, Corp. All rights reserved.
+***
+*** This file is part of Catapult.
+***
+*** Catapult is free software: you can redistribute it and/or modify
+*** it under the terms of the GNU Lesser General Public License as published by
+*** the Free Software Foundation, either version 3 of the License, or
+*** (at your option) any later version.
+***
+*** Catapult is distributed in the hope that it will be useful,
+*** but WITHOUT ANY WARRANTY; without even the implied warranty of
+*** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+*** GNU Lesser General Public License for more details.
+***
+*** You should have received a copy of the GNU Lesser General Public License
+*** along with Catapult. If not, see <http://www.gnu.org/licenses/>.
+**/
+
+#pragma once
+#include "TransactionBuilder.h"
+#include "plugins/txes/account_link/src/model/NodeKeyLinkTransaction.h"
+
+namespace catapult { namespace builders {
+
+	/// Builder for a node key link transaction.
+	class NodeKeyLinkBuilder : public TransactionBuilder {
+	public:
+		using Transaction = model::NodeKeyLinkTransaction;
+		using EmbeddedTransaction = model::EmbeddedNodeKeyLinkTransaction;
+
+	public:
+		/// Creates a node key link builder for building a node key link transaction from \a signer
+		/// for the network specified by \a networkIdentifier.
+		NodeKeyLinkBuilder(model::NetworkIdentifier networkIdentifier, const Key& signer);
+
+	public:
+		/// Sets the linked public key to \a linkedPublicKey.
+		void setLinkedPublicKey(const Key& linkedPublicKey);
+
+		/// Sets the link action to \a linkAction.
+		void setLinkAction(model::LinkAction linkAction);
+
+	public:
+		/// Gets the size of node key link transaction.
+		/// \note This returns size of a normal transaction not embedded transaction.
+		size_t size() const;
+
+		/// Builds a new node key link transaction.
+		std::unique_ptr<Transaction> build() const;
+
+		/// Builds a new embedded node key link transaction.
+		std::unique_ptr<EmbeddedTransaction> buildEmbedded() const;
+
+	private:
+		template<typename TTransaction>
+		size_t sizeImpl() const;
+
+		template<typename TTransaction>
+		std::unique_ptr<TTransaction> buildImpl() const;
+
+	private:
+		Key m_linkedPublicKey;
+		model::LinkAction m_linkAction;
+	};
+}}
