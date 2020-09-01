yilmazbahadir/catapult-server

add FinalizationService, Synchronizer and Api

extensions/finalization/tests/FinalizationServiceTests.cpp
@@ -1,0 +1,105 @@
+/**
+*** Copyright (c) 2016-present,
+*** Jaguar0625, gimre, BloodyRookie, Tech Bureau, Corp. All rights reserved.
+***
+*** This file is part of Catapult.
+***
+*** Catapult is free software: you can redistribute it and/or modify
+*** it under the terms of the GNU Lesser General Public License as published by
+*** the Free Software Foundation, either version 3 of the License, or
+*** (at your option) any later version.
+***
+*** Catapult is distributed in the hope that it will be useful,
+*** but WITHOUT ANY WARRANTY; without even the implied warranty of
+*** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+*** GNU Lesser General Public License for more details.
+***
+*** You should have received a copy of the GNU Lesser General Public License
+*** along with Catapult. If not, see <http://www.gnu.org/licenses/>.
+**/
+
+#include "finalization/src/FinalizationService.h"
+#include "finalization/src/FinalizationBootstrapperService.h"
+#include "tests/test/core/PacketTestUtils.h"
+#include "tests/test/local/PacketWritersServiceTestUtils.h"
+#include "tests/test/local/ServiceTestUtils.h"
+#include "tests/test/net/RemoteAcceptServer.h"
+#include "tests/test/net/mocks/MockPacketWriters.h"
+#include "tests/TestHarness.h"
+
+namespace catapult { namespace finalization {
+
+#define TEST_CLASS FinalizationServiceTests
+
+	namespace {
+		constexpr auto Num_Expected_Tasks = 2u;
+
+		struct FinalizationServiceTraits {
+			static constexpr auto Counter_Name = "FIN WRITERS";
+			static constexpr auto Num_Expected_Services = 2u; // writers (1) + dependent services (1)
+			static constexpr auto CreateRegistrar = CreateFinalizationServiceRegistrar;
+
+			static auto GetWriters(const extensions::ServiceLocator& locator) {
+				return locator.service<net::PacketWriters>("finalization");
+			}
+		};
+
+		class TestContext : public test::ServiceLocatorTestContext<FinalizationServiceTraits> {
+		public:
+			TestContext() {
+				// Arrange: register service dependencies
+				auto pBootstrapperRegistrar = CreateFinalizationBootstrapperServiceRegistrar();
+				pBootstrapperRegistrar->registerServices(locator(), testState().state());
+
+				// - register hook dependencies
+				GetFinalizationServerHooks(locator()).setMessageRangeConsumer([](auto&&) {});
+			}
+		};
+
+		struct Mixin {
+			using TraitsType = FinalizationServiceTraits;
+			using TestContextType = TestContext;
+		};
+	}
+
+	ADD_SERVICE_REGISTRAR_INFO_TEST(Finalization, Post_Extended_Range_Consumers)
+
+	ADD_PACKET_WRITERS_SERVICE_TEST(TEST_CLASS, Mixin, CanBootService)
+	ADD_PACKET_WRITERS_SERVICE_TEST(TEST_CLASS, Mixin, CanShutdownService)
+	ADD_PACKET_WRITERS_SERVICE_TEST(TEST_CLASS, Mixin, CanConnectToExternalServer)
+	ADD_PACKET_WRITERS_SERVICE_TEST(TEST_CLASS, Mixin, WritersAreRegisteredInBannedNodeIdentitySink)
+
+	// region packetIoPickers
+
+	TEST(TEST_CLASS, WritersAreRegisteredInPacketIoPickers) {
+		// Arrange: create a (tcp) server
+		test::RemoteAcceptServer server;
+		server.start();
+
+		// Act: create and boot the service
+		TestContext context;
+		context.boot();
+		auto pickers = context.testState().state().packetIoPickers();
+
+		// - get the packet writers and attempt to connect to the server
+		test::ConnectToLocalHost(*FinalizationServiceTraits::GetWriters(context.locator()), server.caPublicKey());
+
+		// Assert: the writers are registered with role `Voting`
+		EXPECT_EQ(0u, pickers.pickMatching(utils::TimeSpan::FromSeconds(1), ionet::NodeRoles::Peer).size());
+		EXPECT_EQ(1u, pickers.pickMatching(utils::TimeSpan::FromSeconds(1), ionet::NodeRoles::Voting).size());
+	}
+
+	// endregion
+
+	// region tasks
+
+	TEST(TEST_CLASS, ConnectPeersTaskIsScheduled) {
+		test::AssertRegisteredTask(TestContext(), Num_Expected_Tasks, "connect peers task for service Finalization");
+	}
+
+	TEST(TEST_CLASS, PullPtTaskIsScheduled) {
+		test::AssertRegisteredTask(TestContext(), Num_Expected_Tasks, "pull finalization messages task");
+	}
+
+	// endregion
+}}

extensions/finalization/tests/chain/FinalizationMessageSynchronizerTests.cpp
@@ -1,0 +1,114 @@
+/**
+*** Copyright (c) 2016-present,
+*** Jaguar0625, gimre, BloodyRookie, Tech Bureau, Corp. All rights reserved.
+***
+*** This file is part of Catapult.
+***
+*** Catapult is free software: you can redistribute it and/or modify
+*** it under the terms of the GNU Lesser General Public License as published by
+*** the Free Software Foundation, either version 3 of the License, or
+*** (at your option) any later version.
+***
+*** Catapult is distributed in the hope that it will be useful,
+*** but WITHOUT ANY WARRANTY; without even the implied warranty of
+*** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+*** GNU Lesser General Public License for more details.
+***
+*** You should have received a copy of the GNU Lesser General Public License
+*** along with Catapult. If not, see <http://www.gnu.org/licenses/>.
+**/
+
+#include "finalization/src/chain/FinalizationMessageSynchronizer.h"
+#include "finalization/tests/test/mocks/MockFinalizationApi.h"
+#include "tests/test/other/EntitiesSynchronizerTestUtils.h"
+#include "tests/TestHarness.h"
+
+#define DEFINE_MODEL_BINARY_EQUALITY(NAMESPACE, TYPENAME) \
+	namespace catapult { namespace NAMESPACE { \
+		bool operator==(const TYPENAME& lhs, const TYPENAME& rhs); \
+		bool operator==(const TYPENAME& lhs, const TYPENAME& rhs) { \
+			return lhs.Size == rhs.Size && 0 == std::memcmp(&lhs, &rhs, lhs.Size); \
+		} \
+	}}
+
+DEFINE_MODEL_BINARY_EQUALITY(model, FinalizationMessage)
+
+namespace catapult { namespace chain {
+
+	namespace {
+		using MockRemoteApi = mocks::MockFinalizationApi;
+
+		class FinalizationMessageSynchronizerTraits {
+		public:
+			using RequestElementType = utils::ShortHash;
+			using ResponseContainerType = model::AnnotatedEntityRange<model::FinalizationMessage>;
+
+		public:
+			class RemoteApiWrapper {
+			public:
+				explicit RemoteApiWrapper(const api::FinalizationMessageRange& messageRange)
+						: m_pFinalizationApi(std::make_unique<MockRemoteApi>(messageRange))
+				{}
+
+			public:
+				const auto& api() const {
+					return *m_pFinalizationApi;
+				}
+
+				auto numCalls() const {
+					return m_pFinalizationApi->messagesRequests().size();
+				}
+
+				const auto& singleRequest() const {
+					return m_pFinalizationApi->messagesRequests()[0].second;
+				}
+
+				void setError(bool setError = true) {
+					auto entryPoint = setError
+							? MockRemoteApi::EntryPoint::Messages
+							: MockRemoteApi::EntryPoint::None;
+					m_pFinalizationApi->setError(entryPoint);
+				}
+
+				void checkAdditionalRequestParameters() {
+					EXPECT_EQ(crypto::StepIdentifier({ 11, 23, 67 }), m_pFinalizationApi->messagesRequests()[0].first);
+				}
+
+			private:
+				std::unique_ptr<MockRemoteApi> m_pFinalizationApi;
+			};
+
+		public:
+			static auto CreateRequestRange(uint32_t count) {
+				auto shortHashes = test::GenerateRandomDataVector<utils::ShortHash>(count);
+				return model::ShortHashRange::CopyFixed(reinterpret_cast<const uint8_t*>(shortHashes.data()), count);
+			}
+
+			static auto CreateResponseContainer(uint32_t count) {
+				constexpr auto Message_Size = sizeof(model::FinalizationMessage);
+
+				auto buffer = test::GenerateRandomVector(count * Message_Size);
+				for (auto i = 0u; i < count; ++i) {
+					auto& message = reinterpret_cast<model::FinalizationMessage&>(buffer[i * Message_Size]);
+					message.Size = Message_Size;
+					message.HashesCount = 0;
+				}
+
+				return api::FinalizationMessageRange::CopyFixed(buffer.data(), count);
+			}
+
+			static auto CreateRemoteApi(const api::FinalizationMessageRange& messageRange) {
+				return RemoteApiWrapper(messageRange);
+			}
+
+			static auto CreateSynchronizer(
+					const ShortHashesSupplier& shortHashesSupplier,
+					const handlers::MessageRangeHandler& messageRangeConsumer) {
+				auto stepIdentifierSupplier = []() { return crypto::StepIdentifier{ 11, 23, 67 }; };
+				return CreateFinalizationMessageSynchronizer(stepIdentifierSupplier, shortHashesSupplier, messageRangeConsumer);
+			}
+		};
+	}
+
+	DEFINE_ENTITIES_SYNCHRONIZER_TESTS(FinalizationMessageSynchronizer)
+}}

extensions/finalization/tests/test/mocks/MockFinalizationApi.h
@@ -1,0 +1,83 @@
+/**
+*** Copyright (c) 2016-present,
+*** Jaguar0625, gimre, BloodyRookie, Tech Bureau, Corp. All rights reserved.
+***
+*** This file is part of Catapult.
+***
+*** Catapult is free software: you can redistribute it and/or modify
+*** it under the terms of the GNU Lesser General Public License as published by
+*** the Free Software Foundation, either version 3 of the License, or
+*** (at your option) any later version.
+***
+*** Catapult is distributed in the hope that it will be useful,
+*** but WITHOUT ANY WARRANTY; without even the implied warranty of
+*** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+*** GNU Lesser General Public License for more details.
+***
+*** You should have received a copy of the GNU Lesser General Public License
+*** along with Catapult. If not, see <http://www.gnu.org/licenses/>.
+**/
+
+#pragma once
+#include "finalization/src/api/RemoteFinalizationApi.h"
+#include "tests/test/nodeps/Random.h"
+
+namespace catapult { namespace mocks {
+
+	/// Mock finaliztion api that can be configured to return predefined data for requests, capture function parameters
+	/// and throw exceptions at specified entry points.
+	class MockFinalizationApi : public api::RemoteFinalizationApi {
+	public:
+		enum class EntryPoint {
+			None,
+			Messages
+		};
+
+	public:
+		/// Creates a finalization api around a range of messages (\a messageRange).
+		explicit MockFinalizationApi(const api::FinalizationMessageRange& messageRange)
+				: api::RemoteFinalizationApi({ test::GenerateRandomByteArray<Key>(), "fake-host-from-mock-finalization-api" })
+				, m_messageRange(api::FinalizationMessageRange::CopyRange(messageRange))
+				, m_errorEntryPoint(EntryPoint::None)
+		{}
+
+	public:
+		/// Sets the entry point where the exception should occur to \a entryPoint.
+		void setError(EntryPoint entryPoint) {
+			m_errorEntryPoint = entryPoint;
+		}
+
+		/// Gets a vector of parameters that were passed to the messages requests.
+		const auto& messagesRequests() const {
+			return m_messagesRequests;
+		}
+
+	public:
+		/// Gets the configured messages and throws if the error entry point is set to Messages.
+		/// \note The \a stepIdentifier and \a knownShortHashes parameters are captured.
+		thread::future<api::FinalizationMessageRange> messages(
+				const crypto::StepIdentifier& stepIdentifier,
+				model::ShortHashRange&& knownShortHashes) const override {
+			m_messagesRequests.emplace_back(stepIdentifier, std::move(knownShortHashes));
+			if (shouldRaiseException(EntryPoint::Messages))
+				return CreateFutureException<api::FinalizationMessageRange>("messages error has been set");
+
+			return thread::make_ready_future(api::FinalizationMessageRange::CopyRange(m_messageRange));
+		}
+
+	private:
+		bool shouldRaiseException(EntryPoint entryPoint) const {
+			return m_errorEntryPoint == entryPoint;
+		}
+
+		template<typename T>
+		static thread::future<T> CreateFutureException(const char* message) {
+			return thread::make_exceptional_future<T>(catapult_runtime_error(message));
+		}
+
+	private:
+		api::FinalizationMessageRange m_messageRange;
+		EntryPoint m_errorEntryPoint;
+		mutable std::vector<std::pair<crypto::StepIdentifier, model::ShortHashRange>> m_messagesRequests;
+	};
+}}

symbol/symbol

client/catapult/extensions/finalization/tests/FinalizationServiceTests.cpp
@@ -1,0 +1,102 @@
+/**
+*** Copyright (c) 2016-present,
+*** Jaguar0625, gimre, BloodyRookie, Tech Bureau, Corp. All rights reserved.
+***
+*** This file is part of Catapult.
+***
+*** Catapult is free software: you can redistribute it and/or modify
+*** it under the terms of the GNU Lesser General Public License as published by
+*** the Free Software Foundation, either version 3 of the License, or
+*** (at your option) any later version.
+***
+*** Catapult is distributed in the hope that it will be useful,
+*** but WITHOUT ANY WARRANTY; without even the implied warranty of
+*** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+*** GNU Lesser General Public License for more details.
+***
+*** You should have received a copy of the GNU Lesser General Public License
+*** along with Catapult. If not, see <http://www.gnu.org/licenses/>.
+**/
+
+#include "finalization/src/FinalizationService.h"
+#include "finalization/src/FinalizationBootstrapperService.h"
+#include "tests/test/core/PacketTestUtils.h"
+#include "tests/test/local/PacketWritersServiceTestUtils.h"
+#include "tests/test/local/ServiceTestUtils.h"
+#include "tests/test/net/RemoteAcceptServer.h"
+#include "tests/test/net/mocks/MockPacketWriters.h"
+#include "tests/TestHarness.h"
+
+namespace catapult { namespace finalization {
+
+#define TEST_CLASS FinalizationServiceTests
+
+	namespace {
+		struct FinalizationServiceTraits {
+			static constexpr auto Counter_Name = "FIN WRITERS";
+			static constexpr auto Num_Expected_Services = 2u; // writers (1) + dependent services (1)
+			static constexpr auto CreateRegistrar = CreateFinalizationServiceRegistrar;
+
+			static auto GetWriters(const extensions::ServiceLocator& locator) {
+				return locator.service<net::PacketWriters>("fin.writers");
+			}
+		};
+
+		class TestContext : public test::ServiceLocatorTestContext<FinalizationServiceTraits> {
+		public:
+			TestContext() {
+				// Arrange: register service dependencies
+				auto pBootstrapperRegistrar = CreateFinalizationBootstrapperServiceRegistrar();
+				pBootstrapperRegistrar->registerServices(locator(), testState().state());
+
+				// - register hook dependencies
+				GetFinalizationServerHooks(locator()).setMessageRangeConsumer([](auto&&) {});
+			}
+		};
+
+		struct Mixin {
+			using TraitsType = FinalizationServiceTraits;
+			using TestContextType = TestContext;
+		};
+	}
+
+	ADD_SERVICE_REGISTRAR_INFO_TEST(Finalization, Post_Extended_Range_Consumers)
+
+	ADD_PACKET_WRITERS_SERVICE_TEST(TEST_CLASS, Mixin, CanBootService)
+	ADD_PACKET_WRITERS_SERVICE_TEST(TEST_CLASS, Mixin, CanShutdownService)
+	ADD_PACKET_WRITERS_SERVICE_TEST(TEST_CLASS, Mixin, CanConnectToExternalServer)
+	ADD_PACKET_WRITERS_SERVICE_TEST(TEST_CLASS, Mixin, WritersAreRegisteredInBannedNodeIdentitySink)
+
+	// region packetIoPickers
+
+	TEST(TEST_CLASS, WritersAreRegisteredInPacketIoPickers) {
+		// Arrange: create a (tcp) server
+		test::RemoteAcceptServer server;
+		server.start();
+
+		// Act: create and boot the service
+		TestContext context;
+		context.boot();
+		auto pickers = context.testState().state().packetIoPickers();
+
+		// - get the packet writers and attempt to connect to the server
+		test::ConnectToLocalHost(*FinalizationServiceTraits::GetWriters(context.locator()), server.caPublicKey());
+
+		// Assert: the writers are registered with role `Voting`
+		EXPECT_EQ(0u, pickers.pickMatching(utils::TimeSpan::FromSeconds(1), ionet::NodeRoles::Peer).size());
+		EXPECT_EQ(1u, pickers.pickMatching(utils::TimeSpan::FromSeconds(1), ionet::NodeRoles::Voting).size());
+	}
+
+	// endregion
+
+	// region tasks
+
+	TEST(TEST_CLASS, TasksAreRegistered) {
+		test::AssertRegisteredTasks(TestContext(), {
+			"connect peers task for service Finalization",
+			"pull finalization messages task"
+		});
+	}
+
+	// endregion
+}}

client/catapult/extensions/finalization/tests/chain/FinalizationMessageSynchronizerTests.cpp
@@ -1,0 +1,117 @@
+/**
+*** Copyright (c) 2016-present,
+*** Jaguar0625, gimre, BloodyRookie, Tech Bureau, Corp. All rights reserved.
+***
+*** This file is part of Catapult.
+***
+*** Catapult is free software: you can redistribute it and/or modify
+*** it under the terms of the GNU Lesser General Public License as published by
+*** the Free Software Foundation, either version 3 of the License, or
+*** (at your option) any later version.
+***
+*** Catapult is distributed in the hope that it will be useful,
+*** but WITHOUT ANY WARRANTY; without even the implied warranty of
+*** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+*** GNU Lesser General Public License for more details.
+***
+*** You should have received a copy of the GNU Lesser General Public License
+*** along with Catapult. If not, see <http://www.gnu.org/licenses/>.
+**/
+
+#include "finalization/src/chain/FinalizationMessageSynchronizer.h"
+#include "finalization/tests/test/mocks/MockFinalizationApi.h"
+#include "tests/test/other/EntitiesSynchronizerTestUtils.h"
+#include "tests/TestHarness.h"
+
+#define DEFINE_MODEL_BINARY_EQUALITY(NAMESPACE, TYPENAME) \
+	namespace catapult { namespace NAMESPACE { \
+		bool operator==(const TYPENAME& lhs, const TYPENAME& rhs); \
+		bool operator==(const TYPENAME& lhs, const TYPENAME& rhs) { \
+			return lhs.Size == rhs.Size && 0 == std::memcmp(&lhs, &rhs, lhs.Size); \
+		} \
+	}}
+
+DEFINE_MODEL_BINARY_EQUALITY(model, FinalizationMessage)
+
+namespace catapult { namespace chain {
+
+	namespace {
+		using MockRemoteApi = mocks::MockFinalizationApi;
+		using ShortHashesSupplier = supplier<model::ShortHashRange>;
+
+		class FinalizationMessageSynchronizerTraits {
+		public:
+			using RequestElementType = utils::ShortHash;
+			using ResponseContainerType = model::AnnotatedEntityRange<model::FinalizationMessage>;
+
+		public:
+			class RemoteApiWrapper {
+			public:
+				explicit RemoteApiWrapper(const model::FinalizationMessageRange& messageRange)
+						: m_pFinalizationApi(std::make_unique<MockRemoteApi>(messageRange))
+				{}
+
+			public:
+				const auto& api() const {
+					return *m_pFinalizationApi;
+				}
+
+				auto numCalls() const {
+					return m_pFinalizationApi->messagesRequests().size();
+				}
+
+				const auto& singleRequest() const {
+					return m_pFinalizationApi->messagesRequests()[0].second;
+				}
+
+				void setError(bool setError = true) {
+					auto entryPoint = setError
+							? MockRemoteApi::EntryPoint::Messages
+							: MockRemoteApi::EntryPoint::None;
+					m_pFinalizationApi->setError(entryPoint);
+				}
+
+				void checkAdditionalRequestParameters() {
+					EXPECT_EQ(crypto::StepIdentifier({ 11, 23, 67 }), m_pFinalizationApi->messagesRequests()[0].first);
+				}
+
+			private:
+				std::unique_ptr<MockRemoteApi> m_pFinalizationApi;
+			};
+
+		public:
+			static auto CreateRequestRange(uint32_t count) {
+				auto shortHashes = test::GenerateRandomDataVector<utils::ShortHash>(count);
+				return model::ShortHashRange::CopyFixed(reinterpret_cast<const uint8_t*>(shortHashes.data()), count);
+			}
+
+			static auto CreateResponseContainer(uint32_t count) {
+				constexpr auto Message_Size = sizeof(model::FinalizationMessage);
+
+				auto buffer = test::GenerateRandomVector(count * Message_Size);
+				for (auto i = 0u; i < count; ++i) {
+					auto& message = reinterpret_cast<model::FinalizationMessage&>(buffer[i * Message_Size]);
+					message.Size = Message_Size;
+					message.HashesCount = 0;
+				}
+
+				return model::FinalizationMessageRange::CopyFixed(buffer.data(), count);
+			}
+
+			static auto CreateRemoteApi(const model::FinalizationMessageRange& messageRange) {
+				return RemoteApiWrapper(messageRange);
+			}
+
+			static auto CreateSynchronizer(
+					const ShortHashesSupplier& shortHashesSupplier,
+					const handlers::MessageRangeHandler& messageRangeConsumer) {
+				auto messageFilterSupplier = [shortHashesSupplier] {
+					return std::make_pair(crypto::StepIdentifier{ 11, 23, 67 }, shortHashesSupplier());
+				};
+				return CreateFinalizationMessageSynchronizer(messageFilterSupplier, messageRangeConsumer);
+			}
+		};
+	}
+
+	DEFINE_ENTITIES_SYNCHRONIZER_TESTS(FinalizationMessageSynchronizer)
+}}

client/catapult/extensions/finalization/tests/test/mocks/MockFinalizationApi.h
@@ -1,0 +1,83 @@
+/**
+*** Copyright (c) 2016-present,
+*** Jaguar0625, gimre, BloodyRookie, Tech Bureau, Corp. All rights reserved.
+***
+*** This file is part of Catapult.
+***
+*** Catapult is free software: you can redistribute it and/or modify
+*** it under the terms of the GNU Lesser General Public License as published by
+*** the Free Software Foundation, either version 3 of the License, or
+*** (at your option) any later version.
+***
+*** Catapult is distributed in the hope that it will be useful,
+*** but WITHOUT ANY WARRANTY; without even the implied warranty of
+*** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+*** GNU Lesser General Public License for more details.
+***
+*** You should have received a copy of the GNU Lesser General Public License
+*** along with Catapult. If not, see <http://www.gnu.org/licenses/>.
+**/
+
+#pragma once
+#include "finalization/src/api/RemoteFinalizationApi.h"
+#include "tests/test/nodeps/Random.h"
+
+namespace catapult { namespace mocks {
+
+	/// Mock finaliztion api that can be configured to return predefined data for requests, capture function parameters
+	/// and throw exceptions at specified entry points.
+	class MockFinalizationApi : public api::RemoteFinalizationApi {
+	public:
+		enum class EntryPoint {
+			None,
+			Messages
+		};
+
+	public:
+		/// Creates a finalization api around a range of messages (\a messageRange).
+		explicit MockFinalizationApi(const model::FinalizationMessageRange& messageRange)
+				: api::RemoteFinalizationApi({ test::GenerateRandomByteArray<Key>(), "fake-host-from-mock-finalization-api" })
+				, m_messageRange(model::FinalizationMessageRange::CopyRange(messageRange))
+				, m_errorEntryPoint(EntryPoint::None)
+		{}
+
+	public:
+		/// Sets the entry point where the exception should occur to \a entryPoint.
+		void setError(EntryPoint entryPoint) {
+			m_errorEntryPoint = entryPoint;
+		}
+
+		/// Gets a vector of parameters that were passed to the messages requests.
+		const auto& messagesRequests() const {
+			return m_messagesRequests;
+		}
+
+	public:
+		/// Gets the configured messages and throws if the error entry point is set to Messages.
+		/// \note The \a stepIdentifier and \a knownShortHashes parameters are captured.
+		thread::future<model::FinalizationMessageRange> messages(
+				const crypto::StepIdentifier& stepIdentifier,
+				model::ShortHashRange&& knownShortHashes) const override {
+			m_messagesRequests.emplace_back(stepIdentifier, std::move(knownShortHashes));
+			if (shouldRaiseException(EntryPoint::Messages))
+				return CreateFutureException<model::FinalizationMessageRange>("messages error has been set");
+
+			return thread::make_ready_future(model::FinalizationMessageRange::CopyRange(m_messageRange));
+		}
+
+	private:
+		bool shouldRaiseException(EntryPoint entryPoint) const {
+			return m_errorEntryPoint == entryPoint;
+		}
+
+		template<typename T>
+		static thread::future<T> CreateFutureException(const char* message) {
+			return thread::make_exceptional_future<T>(catapult_runtime_error(message));
+		}
+
+	private:
+		model::FinalizationMessageRange m_messageRange;
+		EntryPoint m_errorEntryPoint;
+		mutable std::vector<std::pair<crypto::StepIdentifier, model::ShortHashRange>> m_messagesRequests;
+	};
+}}