75f52806888344d68743c9982aeae51ec36237f8
Jaguar0625 <jaguar0625@127.0.0.1>
2021-03-03 21:51:16 -0400

    add AccountDescriptorRepository and NodeDescriptorRepository yaml read-only readers

---
+++ sdk/pythoncore/NodeDescriptorRepository.py
@@ -0,0 +1,28 @@
+import yaml
+
+
+# pylint: disable=too-few-public-methods
+class NodeDescriptor:
+    """Represents a node."""
+
+    def __init__(self, descriptor_yaml):
+        """Creates a descriptor from a yaml container."""
+        self.host = descriptor_yaml.get('host')
+        self.roles = descriptor_yaml.get('roles') or []
+
+
+class NodeDescriptorRepository:
+    """Loads read-only node descriptors from YAML."""
+
+    def __init__(self, yaml_input):
+        """Loads node descriptors from the specified input."""
+        descriptors_yaml = yaml.load(yaml_input, Loader=yaml.SafeLoader)
+        self.descriptors = [NodeDescriptor(descriptor_yaml) for descriptor_yaml in descriptors_yaml]
+
+    def find_all_by_role(self, role):
+        """Finds all node descriptors with a matching role."""
+        return [descriptor for descriptor in self.descriptors if not role or role in descriptor.roles]
+
+    def find_all_not_by_role(self, role):
+        """Finds all node descriptors without a matching role."""
+        return [descriptor for descriptor in self.descriptors if role and role not in descriptor.roles]
