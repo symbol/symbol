26f29507278077e581d89488f1e60afca8f95f5a
Jaguar0625 <jaguar0625@127.0.0.1>
2021-04-29 11:03:43 -0400

    (build) add commit trigger pipeline

---
+++ client/catapult/scripts/build/jenkins/catapult-server-commit-trigger.groovy
@@ -0,0 +1,26 @@
+pipeline {
+    agent any
+
+    parameters {
+        gitParameter branchFilter: 'origin/(.*)', defaultValue: 'main', name: 'MANUAL_GIT_BRANCH', type: 'PT_BRANCH'
+    }
+
+    stages {
+        stage('build') {
+            steps {
+                script {
+                    dispatch_build_job('gcc-latest', 'tests-metal')
+                }
+            }
+        }
+    }
+}
+
+def dispatch_build_job(compiler_configuration, build_configuration) {
+    git_branch = env.GIT_BRANCH ?: MANUAL_GIT_BRANCH
+    build job: 'server-pipelines/catapult-server-build-catapult-project', parameters: [
+        string(name: 'COMPILER_CONFIGURATION', value: "${compiler_configuration}"),
+        string(name: 'BUILD_CONFIGURATION', value: "${build_configuration}"),
+        gitParameter(name: 'MANUAL_GIT_BRANCH', value: "${git_branch}")
+    ]
+}
