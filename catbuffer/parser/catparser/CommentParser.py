c7af83be0f9dd5786f549a0a14ed1f5417b63be0
Jaguar0625 <jaguar0625@127.0.0.1>
2021-09-01 11:10:51 -0400

    task: convert empty attached comment line to a newline

--- catbuffer/parser/catparser/CommentParser.py
+++ catbuffer/parser/catparser/CommentParser.py
@@ -1,3 +1,7 @@
+# only comments immediately adjacent to a line will appear in the output.
+# consecutive lines are concatenated with <SPACE>.
+# a blank line inserts a new line `\n` in the output.
+
 class CommentParser:
     """Reusable aspect parser for comment statements"""
     def __init__(self):
@@ -13,6 +17,20 @@ class CommentParser:
 
     def commit(self):
         """Postprocesses and clears all captured comments"""
-        comments = ' '.join(comment.strip() for comment in self.comments)
+        comments = ''
+        needs_separator = False
+        for comment in self.comments:
+            comment = comment.strip()
+
+            if not comment:
+                comments += '\n'
+                needs_separator = False
+            else:
+                if needs_separator:
+                    comments += ' '
+
+                comments += comment
+                needs_separator = True
+
         self.comments.clear()
         return {'comments': comments}
