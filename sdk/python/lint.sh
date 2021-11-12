5d4f5f77a0b860a96d5ede65dfd2108cc64e4706
Jaguar0625 <jaguar0625@127.0.0.1>
2021-11-12 11:59:43 -0400

    build: use lint.sh in travis

--- sdk/python/lint.sh
+++ sdk/python/lint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+set -ex
+
 find . -type f -name "*.sh" -print0 | xargs -0 shellcheck
-find . -type f -name "*.py" -print0 | PYTHONPATH=. xargs -0 "$(pyenv which isort)" --check-only --line-length 140
-find . -type f -name "*.py" -print0 | PYTHONPATH=. xargs -0 "$(pyenv which pycodestyle)" --config=.pycodestyle
-find . -type f -name "*.py" -print0 | PYTHONPATH=. xargs -0 "$(pyenv which pylint)" --load-plugins pylint_quotes
+find . -type f -name "*.py" -print0 | PYTHONPATH=. xargs -0 "$(command -v isort)" --check-only --line-length 140
+find . -type f -name "*.py" -print0 | PYTHONPATH=. xargs -0 "$(command -v pycodestyle)" --config=.pycodestyle
+find . -type f -name "*.py" -print0 | PYTHONPATH=. xargs -0 "$(command -v pylint)" --load-plugins pylint_quotes
