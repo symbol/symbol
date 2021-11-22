752a82170250373ab7966988c877430779bd4b9c
Jaguar0625 <jaguar0625@127.0.0.1>
2021-11-22 11:44:52 -0400

    build: use commands directly

--- sdk/python/lint.sh
+++ sdk/python/lint.sh
@@ -3,6 +3,6 @@
 set -ex
 
 find . -type f -name "*.sh" -print0 | xargs -0 shellcheck
-find . -type f -name "*.py" -print0 | PYTHONPATH=. xargs -0 "$(command -v isort)" --check-only --line-length 140
-find . -type f -name "*.py" -print0 | PYTHONPATH=. xargs -0 "$(command -v pycodestyle)" --config=.pycodestyle
-find . -type f -name "*.py" -print0 | PYTHONPATH=. xargs -0 "$(command -v pylint)" --load-plugins pylint_quotes
+find . -type f -name "*.py" -print0 | PYTHONPATH=. xargs -0 python3 -m isort --check-only --line-length 140
+find . -type f -name "*.py" -print0 | PYTHONPATH=. xargs -0 python3 -m pycodestyle --config=.pycodestyle
+find . -type f -name "*.py" -print0 | PYTHONPATH=. xargs -0 python3 -m pylint --load-plugins pylint_quotes
