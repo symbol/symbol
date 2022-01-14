4b24ef3194859655d6b51d44dcc7152424aee2bb
gimre <g.imre@protonmail.com>
2022-01-14 13:20:11 +0100

    [sdk/python] fix: propagate running transaction vectors from travis.yml

--- sdk/python/vectors.sh
+++ sdk/python/vectors.sh
@@ -2,5 +2,7 @@
 
 set -ex
 
+PYTHONPATH=. SCHEMAS_PATH="$(git rev-parse --show-toplevel)/catbuffer/schemas/test-vectors" pytest tests/generator
+
 python3 -m tests.vectors.all --blockchain nem --vectors "$(git rev-parse --show-toplevel)/tests/vectors/nem"
 python3 -m tests.vectors.all --blockchain symbol --vectors "$(git rev-parse --show-toplevel)/tests/vectors/symbol"
