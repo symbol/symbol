e77378f3fcb80252be7d547c2c37161ee2114db9
gimre <g.imre@protonmail.com>
2022-01-17 09:28:22 +0100

    adjust to comment in review #24

--- sdk/python/tests.sh
+++ sdk/python/tests.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-PYTHONPATH=. SCHEMAS_PATH="../../catbuffer/schemas/test-vectors" pytest tests/generator
-
 python3 -m unittest discover -v
 
--- sdk/python/vectors.sh
+++ sdk/python/vectors.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+PYTHONPATH=. SCHEMAS_PATH="../../catbuffer/schemas/test-vectors" pytest tests/generator
+
 python3 -m tests.vectors.all --blockchain nem --vectors ../test-vectors/nem
 python3 -m tests.vectors.all --blockchain symbol --vectors ../test-vectors/symbol
