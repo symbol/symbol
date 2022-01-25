632de798d44997aef263e5dda513751b0b76ba6c
Jaguar0625 <jaguar0625@127.0.0.1>
2022-01-25 08:31:56 -0400

    [monorepo] task: add dependabot.yml

--- linters/scripts/find_space_indents.sh
+++ linters/scripts/find_space_indents.sh
@@ -2,4 +2,11 @@
 
 set -ex
 
-! rg --files-with-matches '^  ' "$(git rev-parse --show-toplevel)" | grep -vE ".json|.md|.LESSER|.eslintrc|.yaml|.txt"
+! rg \
+		--files-with-matches \
+		--type-not=markdown \
+		--type-not=json \
+		--type-not=yaml \
+		--type-not=license \
+		'^  ' "$(git rev-parse --show-toplevel)" \
+	| grep -vE '\.eslintrc|testnet/summary\.txt|.git/hooks/.*\.sample'
--- linters/scripts/run_yamllint.sh
+++ linters/scripts/run_yamllint.sh
@@ -3,5 +3,5 @@
 set -ex
 
 git ls-files "$(git rev-parse --show-toplevel)" \
-	| grep "\.yaml$" \
+	| grep -E '\.(yaml|yml)' \
 	| xargs python3 -m yamllint -c "$(git rev-parse --show-toplevel)/linters/yaml/.yamllint"
