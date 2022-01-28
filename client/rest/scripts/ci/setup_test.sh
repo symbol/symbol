9dbef7e395c409cb11678a2186b539a684686828
wayonb <wayon@symbol.dev>
2022-01-28 17:37:51 -0500

    [client/rest] fix: use tabs
    Update the setup_test.sh with tabs.

--- client/rest/scripts/ci/setup_test.sh
+++ client/rest/scripts/ci/setup_test.sh
@@ -5,13 +5,11 @@ set -ex
 mongod --fork --logpath /data/mongod.log
 
 wait_period=10
-while ! /usr/bin/mongo --eval "printjson(db.version())" > /dev/null 2>&1;
-do
-    sleep 1;
-    let "wait_period-=1"
-    if [ $wait_period -le 1 ];
-    then
-        echo "Waiting for monogo timed out, exiting now.."
-        exit 1
-    fi
+while ! /usr/bin/mongo --eval "printjson(db.version())" >/dev/null 2>&1; do
+	sleep 1
+	((--wait_period))
+	if [ "$wait_period" -le 1 ]; then
+		echo "Waiting for monogo timed out, exiting now.."
+		exit 1
+	fi
 done
