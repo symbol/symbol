3913414e46eff64b8025d5c892e6066d6674a18e
gimre <g.imre@protonmail.com>
2022-02-01 16:21:18 +0100

    slightly more sensible map for deserializer

--- sdk/javascript/generator/FactoryFormatter.py
+++ sdk/javascript/generator/FactoryFormatter.py
@@ -1,3 +1,4 @@
+from catparser.ast import FixedSizeInteger
 from .AbstractTypeFormatter import AbstractTypeFormatter, MethodDescriptor
 from .format import indent
 from .name_formatting import fix_name, underline_name
@@ -30,9 +31,14 @@ class FactoryClassFormatter(ClassFormatter):
 
 	def generate_methods(self):
 		methods = []
+		getters = self.generate_getters()
+		methods.extend(getters)
 		methods.append(self.generate_deserializer())
 		methods.append(self.generate_create_by_name())
 		return methods
+
+	def generate_getters(self):
+		return list(map(self.generate_method, self.provider.get_getter_descriptors()))
 
 
 class FactoryFormatter(AbstractTypeFormatter):
@@ -51,16 +57,21 @@ class FactoryFormatter(AbstractTypeFormatter):
 	def typename(self):
 		return f'{self.abstract.name}Factory'
 
+	def map_to_value(self, name, value, field_type):
+		return f'{name}.{value}' if isinstance(field_type, FixedSizeInteger) else f'{name}.{value}.value'
+
 	def create_discriminator(self, name):
-		field_names = self.factory_descriptor['discriminator_values']
-		values = ', '.join(map(lambda value: f'{name}.{value}.value', field_names))
-		return f'[{values}]: {name}'
+		field_values = self.factory_descriptor['discriminator_values']
+		field_types = self.factory_descriptor['discriminator_types']
+
+		values = ', '.join(map(lambda value_type: self.map_to_value(name, *value_type), zip(field_values, field_types)))
+		return f'[{self.typename}.toKey([{values}]), {name}]'
 
 	def get_deserialize_descriptor(self):
 		body = f'const view = new BufferView(payload);\n';
 		body += f'const {self.printer.name} = {self.printer.load()};\n'
 
-		body += 'const mapping = {\n'
+		body += 'const mapping = new Map([\n'
 
 		if self.factory_descriptor:
 			names = [f'{concrete.name}' for concrete in self.factory_descriptor['children']]
@@ -68,12 +79,14 @@ class FactoryFormatter(AbstractTypeFormatter):
 				',\n'.join(map(self.create_discriminator, names))
 			)
 
-		body += '};\n'
+		body += ']);\n'
 
-		discriminators = [] if not self.factory_descriptor else self.factory_descriptor['discriminator_names']
-		values = ', '.join(map(lambda discriminator: f'{self.printer.name}.{fix_name(discriminator)}', discriminators))
-		body += f'const discriminator = {values};\n'
-		body += 'const factory_class = mapping[discriminator.value];\n'
+		discriminators = [] if not self.factory_descriptor else map(fix_name, self.factory_descriptor['discriminator_names'])
+		discriminator_types = self.factory_descriptor['discriminator_types']
+
+		values = ', '.join(map(lambda value_type: self.map_to_value(self.printer.name, *value_type), zip(discriminators, discriminator_types)))
+		body += f'const discriminator = {self.typename}.toKey([{values}]);\n'
+		body += 'const factory_class = mapping.get(discriminator);\n'
 		body += 'return factory_class.deserialize(view.buffer);'
 
 		return MethodDescriptor(body=body)
@@ -106,7 +119,15 @@ return new mapping[entityName]()
 		raise RuntimeError('not required')
 
 	def get_getter_descriptors(self):
-		return []
+		methods = []
+		body = '''if (1 === values.length)
+	return values[0];
+
+// assume each key is at most 32bits
+return values.map(n => BigInt(n)).reduce((accumulator, value) => (accumulator << 32n) + value);
+'''
+		methods.append(MethodDescriptor(method_name='static toKey', arguments=['values'], body=body))
+		return methods
 
 	def get_str_descriptor(self):
 		raise RuntimeError('not required')
