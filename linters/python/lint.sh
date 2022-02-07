ec37b43eb4087428ef9901be2b8a2855e637aa5b
Jaguar0625 <jaguar0625@127.0.0.1>
2022-02-07 15:17:27 -0400

    [linters] task: disable 'consider-using-f-string' by default

--- linters/python/lint.sh
+++ linters/python/lint.sh
@@ -2,6 +2,10 @@
 
 set -ex
 
+if [ $# -gt 0 ]; then
+	PYLINT_DISABLE_COMMANDS="$1"
+fi
+
 find . -type f -name "*.sh" -print0 | xargs -0 shellcheck
 find . -type f -name "*.py" -print0 | PYTHONPATH=. xargs -0 python3 -m isort \
 	--line-length 140 \
@@ -12,5 +16,5 @@ find . -type f -name "*.py" -print0 | PYTHONPATH=. xargs -0 python3 -m pycodesty
 	--config="$(git rev-parse --show-toplevel)/linters/python/.pycodestyle"
 find . -type f -name "*.py" -print0 | PYTHONPATH=. xargs -0 python3 -m pylint \
 	--rcfile "$(git rev-parse --show-toplevel)/linters/python/.pylintrc" \
-	--disable "consider-using-f-string" \
-	--load-plugins pylint_quotes
+	--load-plugins pylint_quotes \
+	--disable "${PYLINT_DISABLE_COMMANDS}"
--- linters/scripts/ci/lint.sh
+++ linters/scripts/ci/lint.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-bash scripts/ci/lint_python.sh
+bash scripts/ci/lint_python.sh "consider-using-f-string"
 
 "$(git rev-parse --show-toplevel)/linters/scripts/find_space_indents.sh"
 "$(git rev-parse --show-toplevel)/linters/scripts/run_yamllint.sh"
