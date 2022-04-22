7a28fb18b0bb2c2e8164e33bbfda4200bcb26828
wayonb <wayon@symbol.dev>
2022-04-22 14:29:29 -0400

    [monorepo] fix: update codecov parameters
    The latest codecov uploader tool have new parameters.

--- jenkins/shared-library/vars/codeCoverage.groovy
+++ jenkins/shared-library/vars/codeCoverage.groovy
@@ -7,7 +7,7 @@ void uploadCodeCoverage(String flag) {
 	String repoName = env.GIT_URL.tokenize('/').last().split('\\.')[0].toUpperCase()
 	withCredentials([string(credentialsId: "${repoName}_CODECOV_ID", variable: 'CODECOV_TOKEN')]) {
 		logger.logInfo("Uploading code coverage for ${flag}")
-		runScript("codecov --required --root ${env.WORKSPACE} --flags ${flag} .")
+		runScript("codecov --verbose --nonZero --rootDir ${env.WORKSPACE} --flags ${flag} --dir .")
 	}
 }
 
