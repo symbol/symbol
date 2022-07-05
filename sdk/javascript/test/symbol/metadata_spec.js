c364f16663314e6bcc55b74c0f849d2eb3f2fb8b
Jaguar0625 <jaguar0625@127.0.0.1>
2022-07-05 15:05:08 -0400

    [sdk/javascript]: add helper function for calculating metadata payload
     problem: user manually has to do XOR
    solution: add metadataUpdateValue to simplify
       issue: #297

---
+++ sdk/javascript/test/symbol/metadata_spec.js
@@ -0,0 +1,57 @@
+const { metadataUpdateValue } = require('../../src/symbol/metadata');
+const { expect } = require('chai');
+
+describe('metadata', () => {
+	describe('metadataUpdateValue', () => {
+		it('can set new value without old value', () => {
+			// Arrange:
+			const newValue = new Uint8Array([0x9A, 0xC7, 0x33, 0x18, 0xA7, 0xB0, 0x36]);
+
+			// Act:
+			const result = metadataUpdateValue(undefined, newValue);
+
+			// Assert:
+			expect(result).to.deep.equal(newValue);
+		});
+
+		it('can update equal length value', () => {
+			// Arrange:
+			const oldValue = new Uint8Array([0x9A, 0xC7, 0x33, 0x18, 0xA7, 0xB0, 0x36]);
+			const newValue = new Uint8Array([0xD4, 0x60, 0x82, 0xF8, 0x78, 0xFE, 0x78]);
+
+			// Act:
+			const result = metadataUpdateValue(oldValue, newValue);
+
+			// Assert:
+			expect(result).to.deep.equal(new Uint8Array([
+				0x9A ^ 0xD4, 0xC7 ^ 0x60, 0x33 ^ 0x82, 0x18 ^ 0xF8, 0xA7 ^ 0x78, 0xB0 ^ 0xFE, 0x36 ^ 0x78
+			]));
+		});
+
+		it('can update shorter value', () => {
+			// Arrange:
+			const oldValue = new Uint8Array([0x9A, 0xC7, 0x33, 0x18, 0xA7, 0xB0, 0x36]);
+			const newValue = new Uint8Array([0xD4, 0x60, 0x82, 0xF8]);
+
+			// Act:
+			const result = metadataUpdateValue(oldValue, newValue);
+
+			// Assert:
+			expect(result).to.deep.equal(new Uint8Array([0x9A ^ 0xD4, 0xC7 ^ 0x60, 0x33 ^ 0x82, 0x18 ^ 0xF8]));
+		});
+
+		it('can update longer value', () => {
+			// Arrange:
+			const oldValue = new Uint8Array([0x9A, 0xC7, 0x33, 0x18, 0xA7, 0xB0, 0x36]);
+			const newValue = new Uint8Array([0xD4, 0x60, 0x82, 0xF8, 0x78, 0xFE, 0x78, 0xE6, 0x9D, 0xD6]);
+
+			// Act:
+			const result = metadataUpdateValue(oldValue, newValue);
+
+			// Assert:
+			expect(result).to.deep.equal(new Uint8Array([
+				0x9A ^ 0xD4, 0xC7 ^ 0x60, 0x33 ^ 0x82, 0x18 ^ 0xF8, 0xA7 ^ 0x78, 0xB0 ^ 0xFE, 0x36 ^ 0x78, 0xE6, 0x9D, 0xD6
+			]));
+		});
+	});
+});
