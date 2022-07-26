957ef6b9682da2ed00b321dee3910a1b3d70780e
Jaguar0625 <jaguar0625@127.0.0.1>
2022-07-26 19:30:02 -0400

    [sdk/javascript]: add VotingKeysGenerator
     problem: JS sdk doesn't have voting keys generator but PY sdk does
    solution: add voting keys generator to JS sdk to achieve parity

--- sdk/python/symbolchain/symbol/VotingKeysGenerator.py
+++ sdk/python/symbolchain/symbol/VotingKeysGenerator.py
@@ -14,14 +14,14 @@ class VotingKeysGenerator:
 	def generate(self, start_epoch, end_epoch):
 		"""Generates voting keys for specified epochs."""
 		writer = BufferWriter()
-		writer.write_int(start_epoch, 8)
-		writer.write_int(end_epoch, 8)
-		writer.write_int(0xFFFFFFFFFFFFFFFF, 8)
-		writer.write_int(0xFFFFFFFFFFFFFFFF, 8)
+		writer.write_int(start_epoch, 8)  # start key identifier
+		writer.write_int(end_epoch, 8)  # end key identifier
+		writer.write_int(0xFFFFFFFFFFFFFFFF, 8)  # reserved - last (used) key identifier
+		writer.write_int(0xFFFFFFFFFFFFFFFF, 8)  # reserved - last wiped key identifier
 
-		writer.write_bytes(self.root_key_pair.public_key.bytes)
-		writer.write_int(start_epoch, 8)
-		writer.write_int(end_epoch, 8)
+		writer.write_bytes(self.root_key_pair.public_key.bytes)  # root voting public key
+		writer.write_int(start_epoch, 8)  # level 1/1 start key identifier
+		writer.write_int(end_epoch, 8)  # level 1/1 end key identifier
 
 		for identifier in reversed(range(start_epoch, end_epoch + 1)):
 			child_private_key = self.private_key_generator()
@@ -32,7 +32,7 @@ class VotingKeysGenerator:
 			parent_signed_payload_writer.write_int(identifier, 8)
 			signature = self.root_key_pair.sign(parent_signed_payload_writer.buffer)
 
-			writer.write_bytes(child_key_pair.private_key.bytes)
-			writer.write_bytes(signature.bytes)
+			writer.write_bytes(child_key_pair.private_key.bytes)  # child voting private key used to sign votes for an epoch
+			writer.write_bytes(signature.bytes)  # signature proving derivation of child key pair from root
 
 		return writer.buffer
