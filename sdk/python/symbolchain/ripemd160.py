fb3a30a8cd1374d2e9cf0178f20e6ed3a4f07bf4
gimre <gimre@symbol.dev>
2022-08-26 15:00:35 +0200

    [sdk/python]: add small ripemd160 wrapper and pick either hashlib impl or ripemd-hash impl

---
+++ sdk/python/symbolchain/ripemd160.py
@@ -0,0 +1,16 @@
+import hashlib
+
+if 'ripemd160' not in hashlib.algorithms_available:
+	from ripemd import ripemd160 as ripemd160_impl
+
+
+def _factory():
+	return hashlib.new('ripemd160') if 'ripemd160' in hashlib.algorithms_available else ripemd160_impl.new()
+
+
+def ripemd160(data):
+	"""Calculates RIPEMD-160 hash of data."""
+
+	builder = _factory()
+	builder.update(data)
+	return builder.digest()
