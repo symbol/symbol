afccb1e621be1910d516f485116c6add11bf83b6
gimre <gimre@symbol.dev>
2022-08-26 15:00:35 +0200

    [sdk/python]: add small ripemd160 wrapper and pick either hashlib impl or ripemd-hash impl

--- sdk/python/symbolchain/Transforms.py
+++ sdk/python/symbolchain/Transforms.py
@@ -1,11 +1,9 @@
-import hashlib
-
 import sha3
+
+from symbolchain.ripemd160 import ripemd160
 
 
 def ripemd_keccak_256(payload):
 	"""Hashes payload with keccak 256 and then hashes the result with ripemd160."""
 
-	outer_hasher = hashlib.new('ripemd160')
-	outer_hasher.update(sha3.keccak_256(payload).digest())
-	return outer_hasher.digest()
+	return ripemd160(sha3.keccak_256(payload).digest())
