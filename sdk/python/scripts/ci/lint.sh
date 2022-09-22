515e2d96f23d31f409f7e34a01c5815e68188742
gimre <g.imre@protonmail.com>
2022-09-22 13:53:25 +0200

    [sdk/python] build: sdk/python, ignore gitignore-d files

--- sdk/python/scripts/ci/lint.sh
+++ sdk/python/scripts/ci/lint.sh
@@ -3,22 +3,27 @@
 set -ex
 
 find . -type f -name "*.sh" -print0 | xargs -0 shellcheck
-find . -type f -name "*.py" -print0 | PYTHONPATH=. xargs -0 python3 -m isort \
+find . -type f -name "*.py" -print0 | xargs -0 git check-ignore -nv | grep :: | cut -f2- | \
+	PYTHONPATH=. xargs python3 -m isort \
 	--line-length 140 \
 	--indent "	" \
 	--multi-line 3 \
 	--check-only
-find . -name "nc" -prune -o -name "sc" -prune -o -type f -name "*.py" -print0 | PYTHONPATH=. xargs -0 python3 -m pycodestyle \
+find . -name "nc" -prune -o -name "sc" -prune -o -type f -name "*.py" -print0 | xargs -0 git check-ignore -nv | grep :: | cut -f2- | \
+	PYTHONPATH=. xargs python3 -m pycodestyle \
 	--config="$(git rev-parse --show-toplevel)/linters/python/.pycodestyle"
-find . -name "nc" -prune -o -name "sc" -prune -o -type f -name "*.py" -print0 | PYTHONPATH=".:$(git rev-parse --show-toplevel)/catbuffer/parser" xargs -0 python3 -m pylint \
+find . -name "nc" -prune -o -name "sc" -prune -o -type f -name "*.py" -print0 | xargs -0 git check-ignore -nv | grep :: | cut -f2- | \
+	PYTHONPATH=".:$(git rev-parse --show-toplevel)/catbuffer/parser" xargs python3 -m pylint \
 	--rcfile "$(git rev-parse --show-toplevel)/linters/python/.pylintrc" \
 	--load-plugins pylint_quotes
 
 # generated code with some suppressions
-find . -name "nc" -o -name "sc" -o -type f -name "*.py" -print0 | PYTHONPATH=. xargs -0 python3 -m pycodestyle \
+find . -name "nc" -o -name "sc" -o -type f -name "*.py" -print0 | xargs -0 git check-ignore -nv | grep :: | cut -f2- | \
+	PYTHONPATH=. xargs python3 -m pycodestyle \
 	--config="$(git rev-parse --show-toplevel)/linters/python/.pycodestyle" \
 	--max-line-length=215
-find . -name "nc" -o -name "sc" -o -type f -name "*.py" -print0 | PYTHONPATH=".:$(git rev-parse --show-toplevel)/catbuffer/parser" xargs -0 python3 -m pylint \
+find . -name "nc" -o -name "sc" -o -type f -name "*.py" -print0 | xargs -0 git check-ignore -nv | grep :: | cut -f2- | \
+	PYTHONPATH=".:$(git rev-parse --show-toplevel)/catbuffer/parser" xargs python3 -m pylint \
 	--rcfile "$(git rev-parse --show-toplevel)/linters/python/.pylintrc" \
 	--load-plugins pylint_quotes \
 	--disable=duplicate-code
