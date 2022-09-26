0ea0b88f6252d8a0f876641db0ca2e8ce5616431
Jaguar0625 <jaguar0625@127.0.0.1>
2022-09-26 07:50:29 -0400

    [sdk] fix: metadata value calculation when new value is shorter
     problem: when shortening a value, the ending of the existing value should be included
    solution: generate correct update value when shortening

--- sdk/python/symbolchain/symbol/Metadata.py
+++ sdk/python/symbolchain/symbol/Metadata.py
@@ -4,9 +4,15 @@ def metadata_update_value(old_value, new_value):
 	if not old_value:
 		return new_value
 
-	result = []
-	for i in range(0, min(len(old_value), len(new_value))):
+	shorter_length = min(len(old_value), len(new_value))
+	longer_length = max(len(old_value), len(new_value))
+	is_new_value_shorter = len(old_value) > len(new_value)
+
+	result = bytearray()
+	for i in range(shorter_length):
 		result.append(old_value[i] ^ new_value[i])
 
-	result += new_value[len(old_value):len(new_value)]
-	return result
+	for i in range(shorter_length, longer_length):
+		result.append((old_value if is_new_value_shorter else new_value)[i])
+
+	return bytes(result)
