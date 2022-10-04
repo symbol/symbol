29800d43f1b7ba4eed1b880da6e885542c23370b
Jaguar0625 <jaguar0625@127.0.0.1>
2022-10-04 16:49:38 -0400

    [sdk/javascript]: add webpack for creating bundled sdk js for browser
     problem: sdk assumes nodejs environment and cannot be used directly in browser
    solution: use webpack to create browser compatable sdk bundle

---
+++ sdk/javascript/webpack.config.js
@@ -0,0 +1,43 @@
+import WasmPackPlugin from '@wasm-tool/wasm-pack-plugin';
+import path from 'path';
+import URL from 'url';
+import webpack from 'webpack';
+
+export default {
+	entry: './src/index.js',
+	mode: process.env.NODE_ENV || 'development',
+	output: {
+		path: path.resolve(path.dirname(URL.fileURLToPath(import.meta.url)), '_build'),
+		filename: 'bundle.js',
+		library: {
+			type: 'module'
+		}
+	},
+
+	resolve: {
+		extensions: ['.js'],
+		fallback: {
+			crypto: 'crypto-browserify',
+			stream: 'stream-browserify'
+		}
+	},
+
+	plugins: [
+		new webpack.ProvidePlugin({
+			process: 'process/browser',
+			Buffer: ['buffer', 'Buffer']
+		}),
+		new WasmPackPlugin({
+			crateDirectory: path.resolve(path.dirname(URL.fileURLToPath(import.meta.url)), 'wasm'),
+			extraArgs: '--no-typescript',
+			outName: 'symbol_crypto_wasm',
+			outDir: path.resolve(path.dirname(URL.fileURLToPath(import.meta.url)), '_build')
+		}),
+		new webpack.NormalModuleReplacementPlugin(/..\/..\/wasm\/pkg\/symbol_crypto_wasm/, '../../_build/symbol_crypto_wasm.js')
+	],
+
+	experiments: {
+		asyncWebAssembly: true,
+		outputModule: true
+	}
+};
