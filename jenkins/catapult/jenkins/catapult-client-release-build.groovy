e9c49ca490278ee849677382b575b37960474f2b
wayonb <wayon@symbol.dev>
2022-10-13 22:14:49 -0400

    [jenkins] fix: private build tag name

--- jenkins/catapult/jenkins/catapult-client-release-build.groovy
+++ jenkins/catapult/jenkins/catapult-client-release-build.groovy
@@ -171,7 +171,7 @@ String resolveImageLabel() {
 
 	friendlyBranchName = friendlyBranchName.replaceAll('/', '-')
 	architecture = resolveArchitectureLabel()
-	gitHash = "${env.GIT_COMMIT}".substring(8)
+	git_hash = "${env.GIT_COMMIT}".substring(0, 8)
 	return "${COMPILER_CONFIGURATION}-${friendlyBranchName}${architecture}-${gitHash}"
 }
 
