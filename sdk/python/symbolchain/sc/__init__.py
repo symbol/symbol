4cbd07ddc877fc2d81a8d8716477d447687ad357
wayonb <wayon@symbol.dev>
2023-02-06 15:18:35 -0500

    [sdk/python] fix: more pylint

--- sdk/python/symbolchain/sc/__init__.py
+++ sdk/python/symbolchain/sc/__init__.py
@@ -14976,7 +14976,7 @@ class BlockFactory:
 			(NormalBlockV1.BLOCK_TYPE): NormalBlockV1,
 			(ImportanceBlockV1.BLOCK_TYPE): ImportanceBlockV1
 		}
-		discriminator = (parent.type_)
+		discriminator = parent.type_
 		factory_class = mapping[discriminator]
 		return factory_class.deserialize(buffer)
 
@@ -15014,7 +15014,7 @@ class ReceiptFactory:
 			(NamespaceDeletedReceipt.RECEIPT_TYPE): NamespaceDeletedReceipt,
 			(NamespaceRentalFeeReceipt.RECEIPT_TYPE): NamespaceRentalFeeReceipt
 		}
-		discriminator = (parent.type_)
+		discriminator = parent.type_
 		factory_class = mapping[discriminator]
 		return factory_class.deserialize(buffer)
 
