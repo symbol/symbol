5c87e6e248f7d787e9837f686c2788e0ebb65300
wayonb <wayon@symbol.dev>
2023-11-06 15:05:28 -0500

    [sdk/python] fix: remove the requirement of faucet for tests problem: Faucet requires a twitter account to request a claim for tokens.          Test automation does not have a twitter account to get tokens from the Faucet. solution: Use a seed account to provide tokens to automated tests.

--- jenkins/shared-library/vars/runTests.groovy
+++ jenkins/shared-library/vars/runTests.groovy
@@ -2,5 +2,7 @@ void call(String scriptFilepath) {
 	logger.logInfo("Running tests ${scriptFilepath}")
 	final String testMode = env.TEST_MODE ?: 'code-coverage'
 	final String architecture = helper.resolveBuildArchitecture()
-	runScript.withBash("${scriptFilepath} ${testMode} ${architecture}")
+	withCredentials([string(credentialsId: 'SEED_ACCOUNT_PRIVATE_KEY_ID', variable: 'SEED_ACCOUNT_PRIVATE_KEY')]) {
+		runScript.withBash("${scriptFilepath} ${testMode} ${architecture}")
+	}
 }
