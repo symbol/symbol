8a811f3098b2c4f29d6b3a719df1983872c2f6f3
Jaguar0625 <jaguar0625@127.0.0.1>
2024-02-09 17:32:34 -0400

    [sdk/javascript]: disable instance of 'import/no-extraneous-dependencies' eslint warning
     problem: warning is raised when importing optional 'symbol-crypto-wasm-node'
    solution: disable warning

--- sdk/javascript/src/impl/ed25519_wasm.js
+++ sdk/javascript/src/impl/ed25519_wasm.js
@@ -1,8 +1,10 @@
 // this file contains implementation details and is not intended to be used directly
 
+/* eslint-disable import/no-extraneous-dependencies */
 import {
 	HashMode, crypto_private_sign, crypto_private_verify, crypto_sign_keypair
 } from 'symbol-crypto-wasm-node';
+/* eslint-enable import/no-extraneous-dependencies */
 
 const CRYPTO_SIGN_BYTES = 64;
 const CRYPTO_SIGN_PUBLICKEYBYTES = 32;
