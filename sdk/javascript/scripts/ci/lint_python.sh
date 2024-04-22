9a147c2e9cd7d070da264557ed8a013b10955c60
wayonb <wayon@symbol.dev>
2024-04-22 11:39:27 -0400

    [sdk/javascript] fix: skip the node_modules for lint problem: python lint script searches the node_modules folder for linting.          some node packages contains python files. solution: skip the node_modules folder for python lint script

--- sdk/javascript/scripts/ci/lint_python.sh
+++ sdk/javascript/scripts/ci/lint_python.sh
@@ -2,16 +2,17 @@
 
 set -ex
 
-find . -type f -name "*.sh" -print0 | xargs -0 shellcheck
-find . -type f -name "*.py" -print0 | PYTHONPATH=. xargs -0 python3 -m isort \
+find . -name node_modules -prune -o -type f -name "*.sh" -print0 | xargs -0 shellcheck
+find . -name node_modules -prune -o -type f -name "*.py" -print0 | PYTHONPATH=. xargs -0 python3 -m isort \
 	--line-length 140 \
 	--indent "	" \
 	--multi-line 3 \
 	--check-only
-find . -type f -name "*.py" -print0 | PYTHONPATH=. xargs -0 python3 -m pycodestyle \
+find . -name node_modules -prune -o -type f -name "*.py" -print0 | PYTHONPATH=. xargs -0 python3 -m pycodestyle \
 	--config="$(git rev-parse --show-toplevel)/linters/python/.pycodestyle"
 
 # Cygwin is install in our Windows images so pick the correct separator if Windows(Msys)
 SEPARATOR="$([ "$(uname -o)" = "Msys" ] && echo ";" || echo ":")"
-find . -type f -name "*.py" -print0 | PYTHONPATH=".${SEPARATOR}$(git rev-parse --show-toplevel)/catbuffer/parser" xargs -0 python3 -m pylint \
+find . -name node_modules -prune -o -type f -name "*.py" -print0 | \
+	PYTHONPATH=".${SEPARATOR}$(git rev-parse --show-toplevel)/catbuffer/parser" xargs -0 python3 -m pylint \
 	--rcfile "$(git rev-parse --show-toplevel)/linters/python/.pylintrc"
