d3e2c279882208fd8e11dbb090c4512a7aaba78b
Jaguar0625 <jaguar0625@127.0.0.1>
2024-05-23 14:27:12 -0400

    [linters]: allow space indentation in reStructuredText files
     problem: space indentation is recommended for rst files
              generated rst files are space indented
    solution: skip tab indentation check for rst files

--- linters/scripts/lint_indents.sh
+++ linters/scripts/lint_indents.sh
@@ -4,9 +4,10 @@ set -ex
 
 ! rg \
 		--files-with-matches \
-		--type-not=markdown \
 		--type-not=json \
-		--type-not=yaml \
 		--type-not=license \
+		--type-not=markdown \
+		--type-not=rst \
+		--type-not=yaml \
 		'^  ' "$(git rev-parse --show-toplevel)" \
 	| grep -vE '\.eslintrc|testnet/summary\.txt|.git/hooks/.*\.sample'
