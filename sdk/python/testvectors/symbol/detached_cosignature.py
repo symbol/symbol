31435a39ae846eb83dbe1950cfab3c3ae07e4446
wayonb <wayon@symbol.dev>
2024-05-30 20:50:29 -0400

    [sdk]: address feedback

--- sdk/python/testvectors/symbol/detached_cosignature.py
+++ sdk/python/testvectors/symbol/detached_cosignature.py
@@ -6,9 +6,9 @@ from symbolchain.sc import DetachedCosignature, Hash256, PublicKey, Signature
 def create_detached_cosignature():
 	cosignature = DetachedCosignature()
 	cosignature.version = 0
-	cosignature.signer_public_key = PublicKey('67fa12789f80766d329c7f687c5c5f889a82f5e9c3e7996ae4ffe48c34299de7')
+	cosignature.signer_public_key = PublicKey('67FA12789F80766D329C7F687C5C5F889A82F5E9C3E7996AE4FFE48C34299DE7')
 	cosignature.signature = Signature(
-		'622c0ca6cc2ec0c48776fc24bf34fb7f4912b3718457a44d41a32dfcd3dbcedd7d2aa65325ed925e86edeae6ab6ca54ed8b4c0dd090ed9db3860d295da9820ed'
+		'622C0CA6CC2EC0C48776FC24BF34FB7F4912B3718457A44D41A32DFCD3DBCEDD7D2AA65325ED925E86EDEAE6AB6CA54ED8B4C0DD090ED9DB3860D295DA9820ED'
 	)
 	cosignature.parent_hash = Hash256(unhexlify('61E0F8B9AB2FE3E008DCE1380FECDAF5BCFB1851247BF990771154177A0B7E78'))
 	return cosignature
