d7e08b440b370c71ad4e9c9d9c2a364706e6ec0a
Jaguar0625 <jaguar0625@127.0.0.1>
2024-06-28 10:20:54 -0400

    [client/rest]: tabify generated rosetta api files to pass linters checks

--- client/rest/src/plugins/rosetta/openApi/model/Transaction.js
+++ client/rest/src/plugins/rosetta/openApi/model/Transaction.js
@@ -22,94 +22,94 @@ import TransactionIdentifier from './TransactionIdentifier.js';
  * @version 1.4.13
  */
 class Transaction {
-    /**
-     * Constructs a new <code>Transaction</code>.
-     * Transactions contain an array of Operations that are attributable to the same TransactionIdentifier.
-     * @alias module:model/Transaction
-     * @param transactionIdentifier {module:model/TransactionIdentifier} 
-     * @param operations {Array.<module:model/Operation>} 
-     */
-    constructor(transactionIdentifier, operations) { 
-        
-        Transaction.initialize(this, transactionIdentifier, operations);
-    }
+	/**
+	 * Constructs a new <code>Transaction</code>.
+	 * Transactions contain an array of Operations that are attributable to the same TransactionIdentifier.
+	 * @alias module:model/Transaction
+	 * @param transactionIdentifier {module:model/TransactionIdentifier}
+	 * @param operations {Array.<module:model/Operation>}
+	 */
+	constructor(transactionIdentifier, operations) {
 
-    /**
-     * Initializes the fields of this object.
-     * This method is used by the constructors of any subclasses, in order to implement multiple inheritance (mix-ins).
-     * Only for internal use.
-     */
-    static initialize(obj, transactionIdentifier, operations) { 
-        obj['transaction_identifier'] = transactionIdentifier;
-        obj['operations'] = operations;
-    }
+		Transaction.initialize(this, transactionIdentifier, operations);
+	}
 
-    /**
-     * Constructs a <code>Transaction</code> from a plain JavaScript object, optionally creating a new instance.
-     * Copies all relevant properties from <code>data</code> to <code>obj</code> if supplied or a new instance if not.
-     * @param {Object} data The plain JavaScript object bearing properties of interest.
-     * @param {module:model/Transaction} obj Optional instance to populate.
-     * @return {module:model/Transaction} The populated <code>Transaction</code> instance.
-     */
-    static constructFromObject(data, obj) {
-        if (data) {
-            obj = obj || new Transaction();
+	/**
+	 * Initializes the fields of this object.
+	 * This method is used by the constructors of any subclasses, in order to implement multiple inheritance (mix-ins).
+	 * Only for internal use.
+	 */
+	static initialize(obj, transactionIdentifier, operations) {
+		obj['transaction_identifier'] = transactionIdentifier;
+		obj['operations'] = operations;
+	}
 
-            if (data.hasOwnProperty('transaction_identifier')) {
-                obj['transaction_identifier'] = TransactionIdentifier.constructFromObject(data['transaction_identifier']);
-            }
-            if (data.hasOwnProperty('operations')) {
-                obj['operations'] = ApiClient.convertToType(data['operations'], [Operation]);
-            }
-            if (data.hasOwnProperty('related_transactions')) {
-                obj['related_transactions'] = ApiClient.convertToType(data['related_transactions'], [RelatedTransaction]);
-            }
-            if (data.hasOwnProperty('metadata')) {
-                obj['metadata'] = ApiClient.convertToType(data['metadata'], Object);
-            }
-        }
-        return obj;
-    }
+	/**
+	 * Constructs a <code>Transaction</code> from a plain JavaScript object, optionally creating a new instance.
+	 * Copies all relevant properties from <code>data</code> to <code>obj</code> if supplied or a new instance if not.
+	 * @param {Object} data The plain JavaScript object bearing properties of interest.
+	 * @param {module:model/Transaction} obj Optional instance to populate.
+	 * @return {module:model/Transaction} The populated <code>Transaction</code> instance.
+	 */
+	static constructFromObject(data, obj) {
+		if (data) {
+			obj = obj || new Transaction();
 
-    /**
-     * Validates the JSON data with respect to <code>Transaction</code>.
-     * @param {Object} data The plain JavaScript object bearing properties of interest.
-     * @return {boolean} to indicate whether the JSON data is valid with respect to <code>Transaction</code>.
-     */
-    static validateJSON(data) {
-        // check to make sure all required properties are present in the JSON string
-        for (const property of Transaction.RequiredProperties) {
-            if (!data.hasOwnProperty(property)) {
-                throw new Error("The required field `" + property + "` is not found in the JSON data: " + JSON.stringify(data));
-            }
-        }
-        // validate the optional field `transaction_identifier`
-        if (data['transaction_identifier']) { // data not null
-          TransactionIdentifier.validateJSON(data['transaction_identifier']);
-        }
-        if (data['operations']) { // data not null
-            // ensure the json data is an array
-            if (!Array.isArray(data['operations'])) {
-                throw new Error("Expected the field `operations` to be an array in the JSON data but got " + data['operations']);
-            }
-            // validate the optional field `operations` (array)
-            for (const item of data['operations']) {
-                Operation.validateJSON(item);
-            };
-        }
-        if (data['related_transactions']) { // data not null
-            // ensure the json data is an array
-            if (!Array.isArray(data['related_transactions'])) {
-                throw new Error("Expected the field `related_transactions` to be an array in the JSON data but got " + data['related_transactions']);
-            }
-            // validate the optional field `related_transactions` (array)
-            for (const item of data['related_transactions']) {
-                RelatedTransaction.validateJSON(item);
-            };
-        }
+			if (data.hasOwnProperty('transaction_identifier')) {
+				obj['transaction_identifier'] = TransactionIdentifier.constructFromObject(data['transaction_identifier']);
+			}
+			if (data.hasOwnProperty('operations')) {
+				obj['operations'] = ApiClient.convertToType(data['operations'], [Operation]);
+			}
+			if (data.hasOwnProperty('related_transactions')) {
+				obj['related_transactions'] = ApiClient.convertToType(data['related_transactions'], [RelatedTransaction]);
+			}
+			if (data.hasOwnProperty('metadata')) {
+				obj['metadata'] = ApiClient.convertToType(data['metadata'], Object);
+			}
+		}
+		return obj;
+	}
 
-        return true;
-    }
+	/**
+	 * Validates the JSON data with respect to <code>Transaction</code>.
+	 * @param {Object} data The plain JavaScript object bearing properties of interest.
+	 * @return {boolean} to indicate whether the JSON data is valid with respect to <code>Transaction</code>.
+	 */
+	static validateJSON(data) {
+		// check to make sure all required properties are present in the JSON string
+		for (const property of Transaction.RequiredProperties) {
+			if (!data.hasOwnProperty(property)) {
+				throw new Error("The required field `" + property + "` is not found in the JSON data: " + JSON.stringify(data));
+			}
+		}
+		// validate the optional field `transaction_identifier`
+		if (data['transaction_identifier']) { // data not null
+		  TransactionIdentifier.validateJSON(data['transaction_identifier']);
+		}
+		if (data['operations']) { // data not null
+			// ensure the json data is an array
+			if (!Array.isArray(data['operations'])) {
+				throw new Error("Expected the field `operations` to be an array in the JSON data but got " + data['operations']);
+			}
+			// validate the optional field `operations` (array)
+			for (const item of data['operations']) {
+				Operation.validateJSON(item);
+			};
+		}
+		if (data['related_transactions']) { // data not null
+			// ensure the json data is an array
+			if (!Array.isArray(data['related_transactions'])) {
+				throw new Error("Expected the field `related_transactions` to be an array in the JSON data but got " + data['related_transactions']);
+			}
+			// validate the optional field `related_transactions` (array)
+			for (const item of data['related_transactions']) {
+				RelatedTransaction.validateJSON(item);
+			};
+		}
+
+		return true;
+	}
 
 
 }
