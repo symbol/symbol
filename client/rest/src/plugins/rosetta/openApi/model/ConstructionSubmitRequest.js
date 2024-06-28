874c08ce723ec52126d4784a46dd0851834e8246
Jaguar0625 <jaguar0625@127.0.0.1>
2024-06-28 10:20:54 -0400

    [client/rest]: tabify generated rosetta api files to pass linters checks

--- client/rest/src/plugins/rosetta/openApi/model/ConstructionSubmitRequest.js
+++ client/rest/src/plugins/rosetta/openApi/model/ConstructionSubmitRequest.js
@@ -20,72 +20,72 @@ import NetworkIdentifier from './NetworkIdentifier.js';
  * @version 1.4.13
  */
 class ConstructionSubmitRequest {
-    /**
-     * Constructs a new <code>ConstructionSubmitRequest</code>.
-     * The transaction submission request includes a signed transaction.
-     * @alias module:model/ConstructionSubmitRequest
-     * @param networkIdentifier {module:model/NetworkIdentifier} 
-     * @param signedTransaction {String} 
-     */
-    constructor(networkIdentifier, signedTransaction) { 
-        
-        ConstructionSubmitRequest.initialize(this, networkIdentifier, signedTransaction);
-    }
+	/**
+	 * Constructs a new <code>ConstructionSubmitRequest</code>.
+	 * The transaction submission request includes a signed transaction.
+	 * @alias module:model/ConstructionSubmitRequest
+	 * @param networkIdentifier {module:model/NetworkIdentifier}
+	 * @param signedTransaction {String}
+	 */
+	constructor(networkIdentifier, signedTransaction) {
 
-    /**
-     * Initializes the fields of this object.
-     * This method is used by the constructors of any subclasses, in order to implement multiple inheritance (mix-ins).
-     * Only for internal use.
-     */
-    static initialize(obj, networkIdentifier, signedTransaction) { 
-        obj['network_identifier'] = networkIdentifier;
-        obj['signed_transaction'] = signedTransaction;
-    }
+		ConstructionSubmitRequest.initialize(this, networkIdentifier, signedTransaction);
+	}
 
-    /**
-     * Constructs a <code>ConstructionSubmitRequest</code> from a plain JavaScript object, optionally creating a new instance.
-     * Copies all relevant properties from <code>data</code> to <code>obj</code> if supplied or a new instance if not.
-     * @param {Object} data The plain JavaScript object bearing properties of interest.
-     * @param {module:model/ConstructionSubmitRequest} obj Optional instance to populate.
-     * @return {module:model/ConstructionSubmitRequest} The populated <code>ConstructionSubmitRequest</code> instance.
-     */
-    static constructFromObject(data, obj) {
-        if (data) {
-            obj = obj || new ConstructionSubmitRequest();
+	/**
+	 * Initializes the fields of this object.
+	 * This method is used by the constructors of any subclasses, in order to implement multiple inheritance (mix-ins).
+	 * Only for internal use.
+	 */
+	static initialize(obj, networkIdentifier, signedTransaction) {
+		obj['network_identifier'] = networkIdentifier;
+		obj['signed_transaction'] = signedTransaction;
+	}
 
-            if (data.hasOwnProperty('network_identifier')) {
-                obj['network_identifier'] = NetworkIdentifier.constructFromObject(data['network_identifier']);
-            }
-            if (data.hasOwnProperty('signed_transaction')) {
-                obj['signed_transaction'] = ApiClient.convertToType(data['signed_transaction'], 'String');
-            }
-        }
-        return obj;
-    }
+	/**
+	 * Constructs a <code>ConstructionSubmitRequest</code> from a plain JavaScript object, optionally creating a new instance.
+	 * Copies all relevant properties from <code>data</code> to <code>obj</code> if supplied or a new instance if not.
+	 * @param {Object} data The plain JavaScript object bearing properties of interest.
+	 * @param {module:model/ConstructionSubmitRequest} obj Optional instance to populate.
+	 * @return {module:model/ConstructionSubmitRequest} The populated <code>ConstructionSubmitRequest</code> instance.
+	 */
+	static constructFromObject(data, obj) {
+		if (data) {
+			obj = obj || new ConstructionSubmitRequest();
 
-    /**
-     * Validates the JSON data with respect to <code>ConstructionSubmitRequest</code>.
-     * @param {Object} data The plain JavaScript object bearing properties of interest.
-     * @return {boolean} to indicate whether the JSON data is valid with respect to <code>ConstructionSubmitRequest</code>.
-     */
-    static validateJSON(data) {
-        // check to make sure all required properties are present in the JSON string
-        for (const property of ConstructionSubmitRequest.RequiredProperties) {
-            if (!data.hasOwnProperty(property)) {
-                throw new Error("The required field `" + property + "` is not found in the JSON data: " + JSON.stringify(data));
-            }
-        }
-        // validate the optional field `network_identifier`
-        if (data['network_identifier']) { // data not null
-          NetworkIdentifier.validateJSON(data['network_identifier']);
-        }
-        // ensure the json data is a string
-        if (data['signed_transaction'] && !(typeof data['signed_transaction'] === 'string' || data['signed_transaction'] instanceof String)) {
-            throw new Error("Expected the field `signed_transaction` to be a primitive type in the JSON string but got " + data['signed_transaction']);
-        }
+			if (data.hasOwnProperty('network_identifier')) {
+				obj['network_identifier'] = NetworkIdentifier.constructFromObject(data['network_identifier']);
+			}
+			if (data.hasOwnProperty('signed_transaction')) {
+				obj['signed_transaction'] = ApiClient.convertToType(data['signed_transaction'], 'String');
+			}
+		}
+		return obj;
+	}
 
-        return true;
-    }
+	/**
+	 * Validates the JSON data with respect to <code>ConstructionSubmitRequest</code>.
+	 * @param {Object} data The plain JavaScript object bearing properties of interest.
+	 * @return {boolean} to indicate whether the JSON data is valid with respect to <code>ConstructionSubmitRequest</code>.
+	 */
+	static validateJSON(data) {
+		// check to make sure all required properties are present in the JSON string
+		for (const property of ConstructionSubmitRequest.RequiredProperties) {
+			if (!data.hasOwnProperty(property)) {
+				throw new Error("The required field `" + property + "` is not found in the JSON data: " + JSON.stringify(data));
+			}
+		}
+		// validate the optional field `network_identifier`
+		if (data['network_identifier']) { // data not null
+		  NetworkIdentifier.validateJSON(data['network_identifier']);
+		}
+		// ensure the json data is a string
+		if (data['signed_transaction'] && !(typeof data['signed_transaction'] === 'string' || data['signed_transaction'] instanceof String)) {
+			throw new Error("Expected the field `signed_transaction` to be a primitive type in the JSON string but got " + data['signed_transaction']);
+		}
+
+		return true;
+	}
 
 
 }
--- client/rest/src/plugins/rosetta/openApi/model/NetworkOptionsResponse.js
+++ client/rest/src/plugins/rosetta/openApi/model/NetworkOptionsResponse.js
@@ -21,72 +21,72 @@ import Version from './Version.js';
  * @version 1.4.13
  */
 class NetworkOptionsResponse {
-    /**
-     * Constructs a new <code>NetworkOptionsResponse</code>.
-     * NetworkOptionsResponse contains information about the versioning of the node and the allowed operation statuses, operation types, and errors.
-     * @alias module:model/NetworkOptionsResponse
-     * @param version {module:model/Version} 
-     * @param allow {module:model/Allow} 
-     */
-    constructor(version, allow) { 
-        
-        NetworkOptionsResponse.initialize(this, version, allow);
-    }
+	/**
+	 * Constructs a new <code>NetworkOptionsResponse</code>.
+	 * NetworkOptionsResponse contains information about the versioning of the node and the allowed operation statuses, operation types, and errors.
+	 * @alias module:model/NetworkOptionsResponse
+	 * @param version {module:model/Version}
+	 * @param allow {module:model/Allow}
+	 */
+	constructor(version, allow) {
 
-    /**
-     * Initializes the fields of this object.
-     * This method is used by the constructors of any subclasses, in order to implement multiple inheritance (mix-ins).
-     * Only for internal use.
-     */
-    static initialize(obj, version, allow) { 
-        obj['version'] = version;
-        obj['allow'] = allow;
-    }
+		NetworkOptionsResponse.initialize(this, version, allow);
+	}
 
-    /**
-     * Constructs a <code>NetworkOptionsResponse</code> from a plain JavaScript object, optionally creating a new instance.
-     * Copies all relevant properties from <code>data</code> to <code>obj</code> if supplied or a new instance if not.
-     * @param {Object} data The plain JavaScript object bearing properties of interest.
-     * @param {module:model/NetworkOptionsResponse} obj Optional instance to populate.
-     * @return {module:model/NetworkOptionsResponse} The populated <code>NetworkOptionsResponse</code> instance.
-     */
-    static constructFromObject(data, obj) {
-        if (data) {
-            obj = obj || new NetworkOptionsResponse();
+	/**
+	 * Initializes the fields of this object.
+	 * This method is used by the constructors of any subclasses, in order to implement multiple inheritance (mix-ins).
+	 * Only for internal use.
+	 */
+	static initialize(obj, version, allow) {
+		obj['version'] = version;
+		obj['allow'] = allow;
+	}
 
-            if (data.hasOwnProperty('version')) {
-                obj['version'] = Version.constructFromObject(data['version']);
-            }
-            if (data.hasOwnProperty('allow')) {
-                obj['allow'] = Allow.constructFromObject(data['allow']);
-            }
-        }
-        return obj;
-    }
+	/**
+	 * Constructs a <code>NetworkOptionsResponse</code> from a plain JavaScript object, optionally creating a new instance.
+	 * Copies all relevant properties from <code>data</code> to <code>obj</code> if supplied or a new instance if not.
+	 * @param {Object} data The plain JavaScript object bearing properties of interest.
+	 * @param {module:model/NetworkOptionsResponse} obj Optional instance to populate.
+	 * @return {module:model/NetworkOptionsResponse} The populated <code>NetworkOptionsResponse</code> instance.
+	 */
+	static constructFromObject(data, obj) {
+		if (data) {
+			obj = obj || new NetworkOptionsResponse();
 
-    /**
-     * Validates the JSON data with respect to <code>NetworkOptionsResponse</code>.
-     * @param {Object} data The plain JavaScript object bearing properties of interest.
-     * @return {boolean} to indicate whether the JSON data is valid with respect to <code>NetworkOptionsResponse</code>.
-     */
-    static validateJSON(data) {
-        // check to make sure all required properties are present in the JSON string
-        for (const property of NetworkOptionsResponse.RequiredProperties) {
-            if (!data.hasOwnProperty(property)) {
-                throw new Error("The required field `" + property + "` is not found in the JSON data: " + JSON.stringify(data));
-            }
-        }
-        // validate the optional field `version`
-        if (data['version']) { // data not null
-          Version.validateJSON(data['version']);
-        }
-        // validate the optional field `allow`
-        if (data['allow']) { // data not null
-          Allow.validateJSON(data['allow']);
-        }
+			if (data.hasOwnProperty('version')) {
+				obj['version'] = Version.constructFromObject(data['version']);
+			}
+			if (data.hasOwnProperty('allow')) {
+				obj['allow'] = Allow.constructFromObject(data['allow']);
+			}
+		}
+		return obj;
+	}
 
-        return true;
-    }
+	/**
+	 * Validates the JSON data with respect to <code>NetworkOptionsResponse</code>.
+	 * @param {Object} data The plain JavaScript object bearing properties of interest.
+	 * @return {boolean} to indicate whether the JSON data is valid with respect to <code>NetworkOptionsResponse</code>.
+	 */
+	static validateJSON(data) {
+		// check to make sure all required properties are present in the JSON string
+		for (const property of NetworkOptionsResponse.RequiredProperties) {
+			if (!data.hasOwnProperty(property)) {
+				throw new Error("The required field `" + property + "` is not found in the JSON data: " + JSON.stringify(data));
+			}
+		}
+		// validate the optional field `version`
+		if (data['version']) { // data not null
+		  Version.validateJSON(data['version']);
+		}
+		// validate the optional field `allow`
+		if (data['allow']) { // data not null
+		  Allow.validateJSON(data['allow']);
+		}
+
+		return true;
+	}
 
 
 }
