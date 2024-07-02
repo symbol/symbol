92554d3b1e1ca9f93f259c2906557f02ad5a2a79
Jaguar0625 <jaguar0625@127.0.0.1>
2024-07-02 12:48:29 -0400

    [client/rest]: add remaining construction apis #1136
     problem: rosetta requires /construction/* api
    solution: add initial implementations of:
              /construction/payloads
              /construction/combine
              /construction/parse
              /construction/hash
              /construction/submit

--- client/rest/src/plugins/rosetta/constructionRoutes.js
+++ client/rest/src/plugins/rosetta/constructionRoutes.js
@@ -20,37 +20,61 @@
  */
 
 import AccountIdentifier from './openApi/model/AccountIdentifier.js';
+import Amount from './openApi/model/Amount.js';
+import ConstructionCombineRequest from './openApi/model/ConstructionCombineRequest.js';
+import ConstructionCombineResponse from './openApi/model/ConstructionCombineResponse.js';
 import ConstructionDeriveRequest from './openApi/model/ConstructionDeriveRequest.js';
 import ConstructionDeriveResponse from './openApi/model/ConstructionDeriveResponse.js';
+import ConstructionHashRequest from './openApi/model/ConstructionHashRequest.js';
 import ConstructionMetadataRequest from './openApi/model/ConstructionMetadataRequest.js';
 import ConstructionMetadataResponse from './openApi/model/ConstructionMetadataResponse.js';
+import ConstructionParseRequest from './openApi/model/ConstructionParseRequest.js';
+import ConstructionParseResponse from './openApi/model/ConstructionParseResponse.js';
+import ConstructionPayloadsRequest from './openApi/model/ConstructionPayloadsRequest.js';
+import ConstructionPayloadsResponse from './openApi/model/ConstructionPayloadsResponse.js';
 import ConstructionPreprocessRequest from './openApi/model/ConstructionPreprocessRequest.js';
 import ConstructionPreprocessResponse from './openApi/model/ConstructionPreprocessResponse.js';
+import ConstructionSubmitRequest from './openApi/model/ConstructionSubmitRequest.js';
+import Currency from './openApi/model/Currency.js';
+import CurveType from './openApi/model/CurveType.js';
+import Operation from './openApi/model/Operation.js';
+import OperationIdentifier from './openApi/model/OperationIdentifier.js';
+import SignatureType from './openApi/model/SignatureType.js';
+import SigningPayload from './openApi/model/SigningPayload.js';
+import TransactionIdentifier from './openApi/model/TransactionIdentifier.js';
+import TransactionIdentifierResponse from './openApi/model/TransactionIdentifierResponse.js';
 import { RosettaErrorFactory, rosettaPostRouteWithNetwork } from './rosettaUtils.js';
-import { NetworkLocator, PublicKey } from 'symbol-sdk';
-import { Network } from 'symbol-sdk/symbol';
+import { NetworkLocator, PublicKey, utils } from 'symbol-sdk';
+import {
+	Address, Network, NetworkTimestamp, SymbolFacade, generateMosaicAliasId, models
+} from 'symbol-sdk/symbol';
 
 export default {
 	register: (server, db, services) => {
 		const networkName = services.config.network.name;
 		const network = NetworkLocator.findByName(Network.NETWORKS, networkName);
+		const facade = new SymbolFacade(network);
 
 		const restUrl = `${services.config.rest.protocol}://localhost:${services.config.rest.port}`;
 
-		server.post('/construction/derive', rosettaPostRouteWithNetwork(networkName, ConstructionDeriveRequest, typedRequest => {
-			if ('edwards25519' !== typedRequest.public_key.curve_type)
+		const parsePublicKey = rosettaPublicKey => {
+			if (new CurveType().edwards25519 !== rosettaPublicKey.curve_type)
 				throw RosettaErrorFactory.UNSUPPORTED_CURVE;
 
 			try {
-				const publicKey = new PublicKey(typedRequest.public_key.hex_bytes);
-				const address = network.publicKeyToAddress(publicKey);
-
-				const response = new ConstructionDeriveResponse();
-				response.account_identifier = new AccountIdentifier(address.toString());
-				return response;
+				return new PublicKey(rosettaPublicKey.hex_bytes);
 			} catch (err) {
 				throw RosettaErrorFactory.INVALID_PUBLIC_KEY;
 			}
+		};
+
+		server.post('/construction/derive', rosettaPostRouteWithNetwork(networkName, ConstructionDeriveRequest, typedRequest => {
+			const publicKey = parsePublicKey(typedRequest.public_key);
+			const address = network.publicKeyToAddress(publicKey);
+
+			const response = new ConstructionDeriveResponse();
+			response.account_identifier = new AccountIdentifier(address.toString());
+			return response;
 		}));
 
 		server.post('/construction/preprocess', rosettaPostRouteWithNetwork(networkName, ConstructionPreprocessRequest, typedRequest => {
@@ -70,9 +94,9 @@ export default {
 			return response;
 		}));
 
-		const fetchWrapper = async (urlPath, jsonProjection) => {
+		const fetchWrapper = async (urlPath, jsonProjection, requestOptions = {}) => {
 			try {
-				const response = await fetch(`${restUrl}/${urlPath}`);
+				const response = await fetch(`${restUrl}/${urlPath}`, requestOptions);
 				if (!response.ok)
 					throw RosettaErrorFactory.CONNECTION_ERROR;
 
@@ -100,6 +124,232 @@ export default {
 				feeMultiplier: results[1]
 			};
 			return response;
+		}));
+
+		const buildAddressToPublicKeyMap = rosettaPublicKeys => {
+			const addressToPublicKeyMap = {};
+			for (let i = 0; i < rosettaPublicKeys.length; ++i) {
+				const publicKey = parsePublicKey(rosettaPublicKeys[i]);
+				const address = facade.network.publicKeyToAddress(publicKey);
+				addressToPublicKeyMap[address.toString()] = publicKey;
+			}
+
+			return addressToPublicKeyMap;
+		};
+
+		const processTransfer = (addressToPublicKeyMap, subOperation1, subOperation2) => {
+			const makeDescriptor = (senderAccount, recipientAccount, amount) => ({
+				type: 'transfer_transaction_v1',
+				signerPublicKey: addressToPublicKeyMap[senderAccount.address],
+				recipientAddress: recipientAccount.address,
+				mosaics: [
+					{ mosaicId: generateMosaicAliasId('symbol.xym'), amount }
+				]
+			});
+
+			const amount = BigInt(subOperation1.amount.value);
+			if (0n !== amount + BigInt(subOperation2.amount.value))
+				throw RosettaErrorFactory.INVALID_REQUEST_DATA;
+
+			if (0n < amount)
+				return makeDescriptor(subOperation2.account, subOperation1.account, amount);
+
+			return makeDescriptor(subOperation1.account, subOperation2.account, -amount);
+		};
+
+		const createSigningPayload = (signingPayload, signerAddress) => {
+			const result = new SigningPayload(utils.uint8ToHex(signingPayload));
+			result.account_identifier = new AccountIdentifier(signerAddress.toString());
+			result.signature_type = new SignatureType().ed25519;
+			return result;
+		};
+
+		const extractCosignerPublicKeys = (allSignerPublicKeys, aggregateSignerPublicKey) => {
+			const cosignerPublicKeyStringSet = new Set(allSignerPublicKeys.map(publicKey => publicKey.toString()));
+			cosignerPublicKeyStringSet.delete(aggregateSignerPublicKey.toString());
+			return [...cosignerPublicKeyStringSet].sort().map(publicKeyString => new models.PublicKey(publicKeyString));
+		};
+
+		server.post('/construction/payloads', rosettaPostRouteWithNetwork(networkName, ConstructionPayloadsRequest, async typedRequest => {
+			const addressToPublicKeyMap = buildAddressToPublicKeyMap(typedRequest.public_keys);
+
+			const embeddedTransactions = [];
+			const allSignerPublicKeys = [];
+			for (let i = 0; i < typedRequest.operations.length; ++i) {
+				const operation = typedRequest.operations[i];
+
+				if ('transfer' === operation.type) {
+					if (i + 1 === typedRequest.operations.length)
+						throw RosettaErrorFactory.INVALID_REQUEST_DATA;
+
+					const nextOperation = typedRequest.operations[i + 1];
+					if ('transfer' !== nextOperation.type)
+						throw RosettaErrorFactory.INVALID_REQUEST_DATA;
+
+					const embeddedTransaction = facade.transactionFactory.createEmbedded(processTransfer(
+						addressToPublicKeyMap,
+						operation,
+						nextOperation
+					));
+
+					embeddedTransactions.push(embeddedTransaction);
+					allSignerPublicKeys.push(embeddedTransaction.signerPublicKey);
+					++i;
+				} else if ('multisig' === operation.type) {
+					const embeddedTransaction = facade.transactionFactory.createEmbedded({
+						type: 'multisig_account_modification_transaction_v1',
+						signerPublicKey: addressToPublicKeyMap[operation.account.address],
+						...operation.metadata
+					});
+
+					embeddedTransactions.push(embeddedTransaction);
+					allSignerPublicKeys.push(embeddedTransaction.signerPublicKey);
+				} else if ('cosign' === operation.type) {
+					allSignerPublicKeys.push(new models.PublicKey(addressToPublicKeyMap[operation.account.address].bytes));
+				}
+			}
+
+			const aggregateSignerPublicKey = new PublicKey(embeddedTransactions[0].signerPublicKey.bytes);
+			const cosignerPublicKeys = extractCosignerPublicKeys(allSignerPublicKeys, aggregateSignerPublicKey);
+
+			const merkleHash = facade.static.hashEmbeddedTransactions(embeddedTransactions);
+			const aggregateTransaction = facade.transactionFactory.create({
+				type: 'aggregate_complete_transaction_v2',
+				signerPublicKey: aggregateSignerPublicKey,
+				deadline: new NetworkTimestamp(typedRequest.metadata.networkTime).addHours(1).timestamp,
+				transactionsHash: merkleHash,
+				transactions: embeddedTransactions,
+
+				cosignatures: cosignerPublicKeys.map(cosignerPublicKey => {
+					const cosignature = new models.Cosignature();
+					cosignature.signerPublicKey = cosignerPublicKey;
+					return cosignature;
+				})
+			});
+
+			const transactionSize = aggregateTransaction.size;
+			aggregateTransaction.fee = new models.Amount(transactionSize * typedRequest.metadata.feeMultiplier);
+
+			const transactionHash = facade.hashTransaction(aggregateTransaction);
+			const transactionPayload = aggregateTransaction.serialize();
+			const signingPayload = facade.extractSigningPayload(aggregateTransaction);
+
+			const response = new ConstructionPayloadsResponse();
+			response.unsigned_transaction = utils.uint8ToHex(transactionPayload);
+			response.payloads = [
+				createSigningPayload(signingPayload, network.publicKeyToAddress(aggregateTransaction.signerPublicKey)),
+				...cosignerPublicKeys.map(publicKey => createSigningPayload(
+					transactionHash.bytes,
+					network.publicKeyToAddress(publicKey)
+				))
+			];
+
+			return response;
+		}));
+
+		server.post('/construction/combine', rosettaPostRouteWithNetwork(networkName, ConstructionCombineRequest, async typedRequest => {
+			const findSignature = publicKey => typedRequest.signatures.find(rosettaSignature =>
+				0 === utils.deepCompare(publicKey.bytes, utils.hexToUint8(rosettaSignature.public_key.hex_bytes))).hex_bytes;
+
+			const aggregateTransaction = facade.transactionFactory.static.deserialize(utils.hexToUint8(typedRequest.unsigned_transaction));
+			aggregateTransaction.signature = new models.Signature(findSignature(aggregateTransaction.signerPublicKey));
+
+			aggregateTransaction.cosignatures.forEach(cosignature => {
+				cosignature.signature = new models.Signature(findSignature(cosignature.signerPublicKey));
+			});
+
+			const transactionPayload = aggregateTransaction.serialize();
+
+			const response = new ConstructionCombineResponse();
+			response.signed_transaction = utils.uint8ToHex(transactionPayload);
+			return response;
+		}));
+
+		server.post('/construction/parse', rosettaPostRouteWithNetwork(networkName, ConstructionParseRequest, async typedRequest => {
+			const xymCurrency = new Currency('symbol.xym', 6);
+
+			const modelAddressToString = address => new Address(address.bytes).toString();
+			const publicKeyToAccountIdentifier = publicKey =>
+				new AccountIdentifier(facade.network.publicKeyToAddress(publicKey).toString());
+
+			let id = 0;
+			const operations = [];
+			const allSignerPublicKeyStringSet = new Set();
+			const aggregateTransaction = facade.transactionFactory.static.deserialize(utils.hexToUint8(typedRequest.transaction));
+			aggregateTransaction.transactions.forEach(transaction => {
+				allSignerPublicKeyStringSet.add(transaction.signerPublicKey.toString());
+
+				if (models.TransactionType.TRANSFER.value === transaction.type.value) {
+					const amount = transaction.mosaics[0].amount.value;
+
+					const sendOperation = new Operation(new OperationIdentifier(id++), 'transfer');
+					sendOperation.account = publicKeyToAccountIdentifier(transaction.signerPublicKey);
+					sendOperation.amount = new Amount((-amount).toString(), xymCurrency);
+					operations.push(sendOperation);
+
+					const receiveOperation = new Operation(new OperationIdentifier(id++), 'transfer');
+					receiveOperation.account = new AccountIdentifier(modelAddressToString(transaction.recipientAddress));
+					receiveOperation.amount = new Amount(amount.toString(), xymCurrency);
+					operations.push(receiveOperation);
+				} else if (models.TransactionType.MULTISIG_ACCOUNT_MODIFICATION.value === transaction.type.value) {
+					const operation = new Operation(new OperationIdentifier(id++), 'multisig');
+					operation.account = publicKeyToAccountIdentifier(transaction.signerPublicKey);
+					operation.metadata = {
+						minApprovalDelta: transaction.minApprovalDelta,
+						minRemovalDelta: transaction.minRemovalDelta,
+						addressAdditions: transaction.addressAdditions.map(modelAddressToString),
+						addressDeletions: transaction.addressDeletions.map(modelAddressToString)
+					};
+					operations.push(operation);
+				} else {
+					throw RosettaErrorFactory.NOT_SUPPORTED_ERROR;
+				}
+			});
+
+			aggregateTransaction.cosignatures.forEach(cosignature => {
+				if (allSignerPublicKeyStringSet.has(cosignature.signerPublicKey.toString()))
+					return;
+
+				allSignerPublicKeyStringSet.add(cosignature.signerPublicKey.toString());
+
+				const operation = new Operation(new OperationIdentifier(id++), 'cosign');
+				operation.account = publicKeyToAccountIdentifier(cosignature.signerPublicKey);
+				operations.push(operation);
+			});
+
+			const response = new ConstructionParseResponse();
+			response.operations = operations;
+			response.account_identifier_signers = [];
+			response.signers = [];
+
+			if (typedRequest.signed) {
+				const cosignerPublicKeys = extractCosignerPublicKeys([...allSignerPublicKeyStringSet], '');
+				response.account_identifier_signers = cosignerPublicKeys.map(publicKeyToAccountIdentifier);
+			}
+
+			return response;
+		}));
+
+		const processTransactionHashRequest = typedRequest => {
+			const aggregateTransaction = facade.transactionFactory.static.deserialize(utils.hexToUint8(typedRequest.signed_transaction));
+			const transactionHash = facade.hashTransaction(aggregateTransaction);
+
+			const response = new TransactionIdentifierResponse();
+			response.transaction_identifier = new TransactionIdentifier(transactionHash.toString());
+			return response;
+		};
+
+		server.post('/construction/hash', rosettaPostRouteWithNetwork(networkName, ConstructionHashRequest, async typedRequest =>
+			processTransactionHashRequest(typedRequest)));
+
+		server.post('/construction/submit', rosettaPostRouteWithNetwork(networkName, ConstructionSubmitRequest, async typedRequest => {
+			await fetchWrapper('transactions', jsonObject => jsonObject.message, {
+				method: 'PUT',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify({ payload: typedRequest.signed_transaction })
+			});
+
+			return processTransactionHashRequest(typedRequest);
 		}));
 	}
 };
