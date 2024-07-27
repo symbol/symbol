86434db0b529cbbeef5b8edd6436ab85093ca9c2
Jaguar0625 <jaguar0625@127.0.0.1>
2024-07-27 12:53:35 -0400

    [client/rest]: refactor most of process hosting into server/process
     problem: rosetta is bundled within catapult REST directly
    solution: move rosetta implementation into new mini REST

--- client/rest/test/plugins/rosetta/rosetta_spec.js
+++ client/rest/test/plugins/rosetta/rosetta_spec.js
@@ -38,7 +38,7 @@ describe('rosetta plugin', () => {
 			rosetta.registerRoutes(server, undefined, {
 				config: {
 					network: { name: 'testnet' },
-					rest: { protocol: 'http', port: '3456' },
+					restEndpoint: 'http://localhost:3000',
 					rosetta: { aggregateSignerPrivateKey: 'C7CAC863460EBB139F0B61778A285F31B8E8548B3D2354832946C880735C1F70' }
 				}
 			});
