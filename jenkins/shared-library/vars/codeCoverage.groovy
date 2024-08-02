25248e4c5e000b3a2ace965b99c1414a9d2d9579
wayonb <wayon@symbol.dev>
2024-08-02 13:00:18 -0400

    [jenkins]: add support for golang code coverage problem: no code coverage support for golang solution: add golang checks

--- jenkins/shared-library/vars/codeCoverage.groovy
+++ jenkins/shared-library/vars/codeCoverage.groovy
@@ -43,6 +43,14 @@ void verifyCodeCoverageResult(String tool, Integer minimumCodeCoverage) {
 		'jacoco': { Integer target ->
 			logger.logInfo("Minimum code coverage set in pom is ${readJacocoCoverageLimit()}")
 			runScript('mvn jacoco:check@jacoco-check')
+		},
+		'golang': { Integer target ->
+			logCodeCoverageMinimum(target)
+			runScript('go tool cover -func coverage.out')
+			String coverage = runScript("go tool cover -func coverage.out | grep total | awk '{print substr(\$3, 1, length(\$3)-1)}'", true)
+			if (coverage.toInteger() < target) {
+				throw new RuntimeException("Code coverage is below the minimum threshold of ${target}")
+			}
 		}]
 
 	codeCoverageCommand[tool](minimumCodeCoverage)
