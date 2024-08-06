8493170b2b34c9dc5394ea4c90888ffaa2efafdb
Wayon <wayon@symbol.dev>
2024-08-06 00:32:19 -0400

    [client/catapult] fix: use mongodb forward declaration headers

--- client/catapult/extensions/mongo/src/mappers/MapperInclude.h
+++ client/catapult/extensions/mongo/src/mappers/MapperInclude.h
@@ -21,16 +21,7 @@
 
 #pragma once
 #include "catapult/types.h"
-
-namespace bsoncxx {
-	inline namespace v_noabi {
-		namespace array { class view; }
-		namespace document {
-			class value;
-			class view;
-		}
-		namespace types { struct b_binary; }
-	}
-}
-
-namespace mongocxx { inline namespace v_noabi { class cursor; } }
+#include <bsoncxx/builder/stream/array-fwd.hpp>
+#include <bsoncxx/builder/stream/document.hpp>
+#include <bsoncxx/types-fwd.hpp>
+#include <mongocxx/cursor-fwd.hpp>
