59e530b59ea70d39cce11181a7857708c416a014
Xavi Artigas <xavierartigas@yahoo.es>
2025-02-20 08:42:17 +0100

    WIP

--- docs/scripts/gen_ref_pages_py.py
+++ docs/scripts/gen_ref_pages_py.py
@@ -1,7 +1,11 @@
-"""Generate the Python code reference pages."""
+"""Generate the Python code reference pages.
+Generates them in-place, instead of in a temporary folder as is customary,
+so that the i18n plugin picks up the files.
+"""
 
 from pathlib import Path
 
+import os
 import mkdocs_gen_files
 
 nav = mkdocs_gen_files.Nav()
@@ -12,8 +16,8 @@ paths = sorted(src.rglob("*.py"))
 
 for path in paths:
     module_path = path.relative_to(src.parent).with_suffix("")
-    doc_path = path.relative_to(src).with_suffix(".md")
-    full_doc_path = Path("en/devbook/reference/py", doc_path)
+    doc_path = path.relative_to(src).with_suffix(".en.md")
+    full_doc_path = Path("pages/devbook/reference/py", doc_path)
 
     parts = tuple(module_path.parts)
 
@@ -24,11 +28,14 @@ for path in paths:
 
     nav[parts] = doc_path.as_posix()
 
-    with mkdocs_gen_files.open(full_doc_path, "w") as fd:
+    folder = os.path.split(full_doc_path)[0]
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    with open(full_doc_path, "w") as fd:
         identifier = ".".join(parts)
         print("::: " + identifier, file=fd)
 
     mkdocs_gen_files.set_edit_path(full_doc_path, path.relative_to(root))
 
-with mkdocs_gen_files.open("en/devbook/reference/py/navigation.md", "w") as nav_file:
+with open("pages/devbook/reference/py/navigation.md", "w") as nav_file:
     nav_file.writelines(nav.build_literate_nav())
