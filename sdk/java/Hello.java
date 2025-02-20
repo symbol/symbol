b7f905ee14333514cc7070dcf2d508b26991c79a
Xavi Artigas <xavierartigas@yahoo.es>
2025-02-20 08:42:23 +0100

    Add MkDoxy for Java API docs (WIP)

---
+++ sdk/java/Hello.java
@@ -0,0 +1,21 @@
+/**
+* A minimal *Hello world* program in Java for noobs
+*/
+public class Hello {
+  /**
+  * The main method.
+  * @param args Classic command-line arguments.
+  */
+  public static void main(String args[]) {
+    System.out.println("Hello world");
+  }
+
+  /**
+  * A second method, to test crossrefs.
+  *
+  * The other method is main `main` [main][] main().
+  */
+  public void method1() {
+    System.out.println("Method 1");
+  }
+}
