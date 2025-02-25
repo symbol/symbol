445b82ede955b0185597e3f8f890529d13b791d7
Wayonb <wayon@symbol.dev>
2025-02-24 21:02:03 -0500

    [sdk/javascript] fix: upgrade rustc to supports edition2024 feature
    problem: the latest version of hex-literal requires edition2024 feature
    solution: upgrade to rustc 1.85, which supports edition2024 feature

--- sdk/javascript/scripts/ci/build.sh
+++ sdk/javascript/scripts/ci/build.sh
@@ -7,10 +7,7 @@ bash scripts/run_catbuffer_generator_ts.sh dryrun
 
 # build wasm variants
 cd wasm
-# rustup default stable
-# stay with rustc 1.81.0 until wasm opt is fixed
-# https://github.com/rustwasm/wasm-pack/issues/1441
-rustup install 1.81.0
+rustup default stable
 wasm-pack build --release --no-typescript --target nodejs --out-dir ../_build/wasm/node
 wasm-pack build --release --no-typescript --target web --out-dir ../_build/wasm/web
 cd ..
