e088bb78498d010d105117871b428fb09a009c01
Wayon <wayon@symbol.dev>
2025-03-13 00:52:01 -0400

    mkdocs]: build with docs image

--- mkdocs/scripts/ci/gh_pages_publish.sh
+++ mkdocs/scripts/ci/gh_pages_publish.sh
@@ -5,4 +5,4 @@ set -ex
 git commit -m "[docs]: release new docs"
 git remote set-url origin https://github.com/symbol/symbol
 git remote -v
-git push -f origin symbol:gh-pages
+git push -f origin main:gh-pages
