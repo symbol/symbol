fb663883f19ae20cd9447d3a9118717ae5fd158b
Xavi Artigas <xavierartigas@yahoo.es>
2025-03-14 18:00:11 +0100

    Tabs instead of spaces in Python

--- mkdocs/scripts/gen_ref_pages_py.py
+++ mkdocs/scripts/gen_ref_pages_py.py
@@ -9,24 +9,24 @@ src = root / "sdk/python/symbolchain"
 paths = sorted(src.rglob("*.py"))
 
 for path in paths:
-    module_path = path.relative_to(src.parent).with_suffix("")
-    doc_path = path.relative_to(src).with_suffix(".md")
-    full_doc_path = Path("devbook/reference/py", doc_path)
+	module_path = path.relative_to(src.parent).with_suffix("")
+	doc_path = path.relative_to(src).with_suffix(".md")
+	full_doc_path = Path("devbook/reference/py", doc_path)
 
-    parts = tuple(module_path.parts)
+	parts = tuple(module_path.parts)
 
-    if parts[-1] == "__init__":
-        parts = parts[:-1]
-    elif parts[-1] == "__main__":
-        continue
+	if parts[-1] == "__init__":
+		parts = parts[:-1]
+	elif parts[-1] == "__main__":
+		continue
 
-    nav[parts] = doc_path.as_posix()
+	nav[parts] = doc_path.as_posix()
 
-    with mkdocs_gen_files.open(full_doc_path, "w") as fd:
-        identifier = ".".join(parts)
-        print("::: " + identifier, file=fd)
+	with mkdocs_gen_files.open(full_doc_path, "w") as fd:
+		identifier = ".".join(parts)
+		print("::: " + identifier, file=fd)
 
-    mkdocs_gen_files.set_edit_path(full_doc_path, path.relative_to(root))
+	mkdocs_gen_files.set_edit_path(full_doc_path, path.relative_to(root))
 
 with mkdocs_gen_files.open("devbook/reference/py/links.md", "w") as nav_file:
-    nav_file.writelines(nav.build_literate_nav())
+	nav_file.writelines(nav.build_literate_nav())
