a1894accdd0b7057b1601a3f3729310dc22ccb5c
Wayon <wayon@symbol.dev>
2025-03-19 17:58:23 -0400

    [jenkins]: fix dockerfile path

--- jenkins/catapult/jenkins/catapult-client-release-build.groovy
+++ jenkins/catapult/jenkins/catapult-client-release-build.groovy
@@ -128,7 +128,7 @@ pipeline {
 						script {
 							final String hostName = helper.resolveUrlHostName(dockerUrl)
 							final String defaultUser = 'fedora' == "${params.OPERATING_SYSTEM}" ? 'fedora' : 'ubuntu'
-							final String buildArg = "-f release.Dockerfile " +
+							final String buildArg = "-f jenkins/catapult/templates/release.Dockerfile " +
 								"--build-arg BUILD_IMAGE=${baseImageNames[0]} " +
 								"--build-arg RELEASE_BASE_IMAGE=${baseImageNames[1]} " +
 								"--build-arg BUILD_CONFIGURATION=${BUILD_CONFIGURATION} " +
