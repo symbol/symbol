0a44d75b6ca569f43f7e1a84c3a8266a797998cb
Wayon <wayon@symbol.dev>
2025-03-19 22:18:50 -0400

    [jenkins]: fix image name

--- jenkins/catapult/jenkins/catapult-client-release-build.groovy
+++ jenkins/catapult/jenkins/catapult-client-release-build.groovy
@@ -126,28 +126,26 @@ pipeline {
 				stage('build and push image') {
 					steps {
 						script {
-							final String hostName = helper.resolveUrlHostName(dockerUrl)
 							final String defaultUser = 'fedora' == "${params.OPERATING_SYSTEM}" ? 'fedora' : 'ubuntu'
-							final String buildArg = "-f symbol-mono/jenkins/catapult/templates/release.Dockerfile " +
+							final String buildArg = '-f symbol-mono/jenkins/catapult/templates/release.Dockerfile ' +
 								"--build-arg BUILD_IMAGE=${baseImageNames[0]} " +
 								"--build-arg RELEASE_BASE_IMAGE=${baseImageNames[1]} " +
 								"--build-arg BUILD_CONFIGURATION=${BUILD_CONFIGURATION} " +
 								"--build-arg COMPILER_CONFIGURATION=${COMPILER_CONFIGURATION} " +
 								"--build-arg USER_NAME=${defaultUser} symbol-mono/"
-							final String archImageName = "${hostName}/${dockerRepoName}:${resolveShortArchitectureImageLabel(compilerConfigurationFilepath)}"
+							final String archImageName = "${dockerRepoName}:${resolveShortArchitectureImageLabel(compilerConfigurationFilepath)}"
 
 							dockerHelper.dockerBuildAndPushImage(
 								params.OPERATING_SYSTEM,
-								"${env.DOCKER_URL}",
-								"${env.DOCKER_CREDENTIALS_ID}",
+								dockerUrl,
+								dockerCredentialId,
 								archImageName,
 								buildArg,
 								SHOULD_PUBLISH_BUILD_IMAGE.toBoolean()
 							)
-							dockerHelper.loginAndRunCommand(dockerCredentialId, dockerUrl) {
-								String multiArchImageName = "${hostName}/${dockerRepoName}:${resolveShortImageLabel()}"
-								dockerHelper.updateDockerImage(multiArchImageName, archImageName, "${ARCHITECTURE}")
-							}
+
+							final String multiArchImageName = "${dockerRepoName}:${resolveShortImageLabel()}"
+							dockerHelper.tagDockerImage("${OPERATING_SYSTEM}", dockerUrl, dockerCredentialId, archImageName, multiArchImageName)
 						}
 					}
 				}
