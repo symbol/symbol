d5a23ff2ec428a0f00e3d02cbc9934223f9908f9
Xavi Artigas <xavierartigas@yahoo.es>
2025-03-24 20:15:37 +0100

    [mkdocs] fix: allow links in glossary definitions
    The ezglossary plugin does not allow links (or any HTML tag) in glossary
    definitions, because they need to go into a `name` attribute.
    But instead of replacing links with their text, it just swallows them.
    This commit patches the plugin's installation to keep the link text.
    
    Also, change the plugin config and the template for slightly nicer HTML.

--- mkdocs/scripts/ci/setup_build.sh
+++ mkdocs/scripts/ci/setup_build.sh
@@ -1,8 +1,15 @@
 set -ex
 
+# Install Typedoc
 npm install
 
+# Build Typescript SDK
 pushd ../sdk/javascript
 npm install
 npx tsc -p ./tsconfig/build-bindings.json
 popd
+
+# Patch the ezglossary plugin, ignoring errors if it was already patched
+ez_root=$(pip show mkdocs-ezglossary-plugin | sed -n 's/Location: \(.*\)/\1/p')
+ez_plugin=$ez_root/mkdocs_ezglossary_plugin/plugin.py
+patch $ez_plugin scripts/ci/ezglossary.patch -f -s || true
