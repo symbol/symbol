151ec122d115d2b131cdf8dd933cfc0079f9297a
Xavi Artigas <xavierartigas@yahoo.es>
2025-03-25 10:58:04 +0100

    [mkdocs] fix: lint patch for ezglossary
    The patch for ezglossary does not pass our linting rules, so it needed to
    be adapted and apply the patch ignoring whitespace.

--- mkdocs/scripts/ci/setup_build.sh
+++ mkdocs/scripts/ci/setup_build.sh
@@ -12,4 +12,4 @@ popd
 # Patch the ezglossary plugin, ignoring errors if it was already patched
 ez_root=$(pip show mkdocs-ezglossary-plugin | sed -n 's/Location: \(.*\)/\1/p')
 ez_plugin=$ez_root/mkdocs_ezglossary_plugin/plugin.py
-patch $ez_plugin scripts/ci/ezglossary.patch -f -s || true
+patch $ez_plugin scripts/ci/ezglossary.patch --force --ignore-whitespace --fuzz 0 || true
