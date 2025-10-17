a12342f744d952f7098c50a54bcac9b3893ff102
Xavi Artigas <xavierartigas@yahoo.es>
2025-10-17 10:48:17 +0200

    [mkdocs] feat: add helper script to deploy docs to gh-pages
    EXTREMELY MANUAL
    NO ERROR CHECKING
    USE WITH CAUTION

---
+++ mkdocs/scripts/deploy-gh-pages.sh
@@ -0,0 +1,14 @@
+SYMBOL_DOCS_DISABLE_TS=false mkdocs build -f config/mkdocs.en.yml
+SYMBOL_DOCS_DISABLE_TS=false mkdocs build -f config/mkdocs.ja.yml
+cd ../docs
+mv en en2
+mv ja ja2
+git checkout gh-pages
+rm -rf en ja
+mv en2 en
+mv ja2 ja
+git add -f en ja
+git commit -m "[docs] Update"
+git push
+git checkout new-docs
+cd ../mkdocs
