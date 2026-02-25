7cc972ac029cb88b9aabc93883c59b6d1e4e109c
zero <zero4862@pm.me>
2026-02-25 09:53:32 +0000

    [mkdocs] docs: add resolve namespace from receipt tutorial (#1865)
    * [mkdocs] docs: add resolve namespace from receipt tutorial
    * [mkdocs] docs: address comments
    * [mkdocs] reviewer feedback
    
    ---------
    
    Co-authored-by: Xavi Artigas <xavierartigas@yahoo.es>

---
+++ mkdocs/overrides/devbook/namespaces/resolve-namespace-from-receipt.py
@@ -0,0 +1,102 @@
+import json
+import os
+import urllib.request
+
+from symbolchain.symbol.Network import Address
+
+NODE_URL = os.getenv(
+	'NODE_URL', 'https://reference.symboltest.net:3001')
+print(f'Using node {NODE_URL}')
+
+# Hash of a confirmed tx that used a namespace alias
+TX_HASH = os.getenv('TRANSACTION_HASH',
+	'BA0C65DB752A3BF1B25285540642537ECE8C2CA716577EDF8BF0F8597A85ADC4')
+print(f'Transaction hash: {TX_HASH}')
+
+try:
+	# Retrieve the confirmed transaction
+	tx_path = f'/transactions/confirmed/{TX_HASH}'
+	print(f'Fetching transaction from {tx_path}')
+	with urllib.request.urlopen(f'{NODE_URL}{tx_path}') as response:
+		tx_data = json.loads(response.read().decode())
+
+	block_height = tx_data['meta']['height']
+	print(f'  Block height: {block_height}')
+
+	# primaryId is 1-based, meta.index is 0-based
+	tx_index = int(tx_data['meta']['index'])
+	tx_primary = tx_index + 1
+	print(f'  Transaction index: {tx_index} (primaryId: {tx_primary})')
+
+	recipient_hex = tx_data['transaction']['recipientAddress']
+	recipient_bytes = bytes.fromhex(recipient_hex)
+	is_address_alias = (recipient_bytes[0] & 0x01) == 1
+	print(f'  Recipient: {recipient_hex}')
+	print(f'  Is address alias: {is_address_alias}')
+
+	aliased_mosaics = set()
+	mosaics = tx_data['transaction']['mosaics']
+	for mosaic in mosaics:
+		mosaic_id = int(mosaic['id'], 16)
+		is_mosaic_alias = (mosaic_id >> 63) & 1 == 1
+		if is_mosaic_alias:
+			aliased_mosaics.add(mosaic['id'])
+		print(f'  Mosaic: {mosaic["id"]}')
+		print(f'  Is mosaic alias: {is_mosaic_alias}')
+
+	# Query address resolution statements
+	if is_address_alias:
+		address_path = ('/statements/resolutions/address'
+			f'?height={block_height}')
+		print(f'\nFetching address resolutions from {address_path}')
+		with urllib.request.urlopen(
+				f'{NODE_URL}{address_path}') as response:
+			address_data = json.loads(response.read().decode())
+
+		address_statements = address_data['data']
+		print(f'  Found {len(address_statements)}'
+			+ ' resolution statement(s)')
+
+		for item in address_statements:
+			statement = item['statement']
+			if statement['unresolved'] != recipient_hex:
+				continue
+			resolved = None
+			for entry in statement['resolutionEntries']:
+				source = entry['source']
+				if source['primaryId'] <= tx_primary:
+					resolved = entry['resolved']
+			if resolved:
+				address = Address(bytes.fromhex(resolved))
+				print('\nAddress resolution:')
+				print(f'  Unresolved:  {statement["unresolved"]}')
+				print(f'  Resolved:   {address}')
+
+	# Query mosaic resolution statements
+	if len(aliased_mosaics):
+		mosaic_path = ('/statements/resolutions/mosaic'
+			f'?height={block_height}')
+		print(f'\nFetching mosaic resolutions from {mosaic_path}')
+		with urllib.request.urlopen(
+				f'{NODE_URL}{mosaic_path}') as response:
+			mosaic_data = json.loads(response.read().decode())
+
+		mosaic_statements = mosaic_data['data']
+		print(f'  Found {len(mosaic_statements)} resolution statement(s)')
+
+		for item in mosaic_statements:
+			statement = item['statement']
+			if statement['unresolved'] not in aliased_mosaics:
+				continue
+			resolved = None
+			for entry in statement['resolutionEntries']:
+				source = entry['source']
+				if source['primaryId'] <= tx_primary:
+					resolved = entry['resolved']
+			if resolved:
+				print('\nMosaic resolution:')
+				print(f'  Unresolved: {statement["unresolved"]}')
+				print(f'  Resolved:   {resolved}')
+
+except Exception as e:
+	print(e)
