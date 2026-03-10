afbda879e7562c506058fce6de819a2fe150d45e
zero <zero4862@pm.me>
2026-03-10 09:06:22 +0000

    [mkdocs] docs: add listen to transaction flow tutorial (#1892)
    * [mkdocs] docs: add listen to transaction flow tutorial
    [mkdocs] docs: review tutorial
    [mkdocs] docs: review tutorial
    [mkdocs] docs: review tutorial
    [mkdocs] docs: add tutorial to sidebar
    [mkdocs] docs: add tutorial to sidebar
    * [mkdocs] docs: fix websocket links
    * [mkdocs] docs: address comments
    [mkdocs] docs: address comments
    * [mkdocs] review
    
    ---------
    
    Co-authored-by: Xavi Artigas <xavierartigas@yahoo.es>

---
+++ mkdocs/overrides/devbook/websockets/listen-transaction-flow.py
@@ -0,0 +1,112 @@
+import asyncio
+import json
+import os
+import urllib.request
+
+from symbolchain.CryptoTypes import PrivateKey
+from symbolchain.facade.SymbolFacade import SymbolFacade
+from symbolchain.symbol.Network import NetworkTimestamp
+from symbolchain.sc import Amount
+from websockets import connect
+
+NODE_URL = os.getenv('NODE_URL', 'https://reference.symboltest.net:3001')
+WS_URL = NODE_URL.replace('http', 'ws', 1) + '/ws'
+print(f'Using node {NODE_URL}')
+
+MONITOR_ADDRESS = os.getenv(
+	'MONITOR_ADDRESS',
+	'TCHBDENCLKEBILBPWP3JPB2XNY64OE7PYHHE32I'
+)
+print(f'Monitoring address: {MONITOR_ADDRESS}')
+
+SIGNER_PRIVATE_KEY = os.getenv(
+	'SIGNER_PRIVATE_KEY',
+	'0000000000000000000000000000000000000000000000000000000000000000'
+)
+facade = SymbolFacade('testnet')
+signer_key_pair = SymbolFacade.KeyPair(PrivateKey(SIGNER_PRIVATE_KEY))
+
+
+async def main():
+	async with connect(WS_URL) as websocket:
+		# Connect to WebSocket
+		response = json.loads(await websocket.recv())
+		uid = response['uid']
+		print(f'Connected to {WS_URL} with uid {uid}')
+
+		# Subscribe to transaction channels
+		channels = [
+			f'unconfirmedAdded/{MONITOR_ADDRESS}',
+			f'unconfirmedRemoved/{MONITOR_ADDRESS}',
+			f'confirmedAdded/{MONITOR_ADDRESS}',
+		]
+		for channel in channels:
+			await websocket.send(json.dumps(
+				{'uid': uid, 'subscribe': channel}
+			))
+			name = channel.split('/')[0]
+			print(f'Subscribed to {name} channel')
+
+		# Build and announce a transfer transaction
+		with urllib.request.urlopen(f'{NODE_URL}/node/time') as resp:
+			time_json = json.loads(resp.read().decode())
+			timestamp = NetworkTimestamp(int(
+				time_json['communicationTimestamps']['receiveTimestamp']))
+
+		with urllib.request.urlopen(
+			f'{NODE_URL}/network/fees/transaction'
+		) as resp:
+			fee_json = json.loads(resp.read().decode())
+			fee_mult = max(
+				fee_json['medianFeeMultiplier'],
+				fee_json['minFeeMultiplier'])
+
+		transaction = facade.transaction_factory.create({
+			'type': 'transfer_transaction_v1',
+			'signer_public_key': signer_key_pair.public_key,
+			'deadline': timestamp.add_hours(2).timestamp,
+			'recipient_address': MONITOR_ADDRESS,
+		})
+		transaction.fee = Amount(fee_mult * transaction.size)
+
+		signature = facade.sign_transaction(signer_key_pair, transaction)
+		json_payload = facade.transaction_factory.attach_signature(
+			transaction, signature)
+		transaction_hash = str(facade.hash_transaction(transaction))
+
+		announce_request = urllib.request.Request(
+			f'{NODE_URL}/transactions',
+			data=json_payload.encode(),
+			headers={'Content-Type': 'application/json'},
+			method='PUT'
+		)
+		with urllib.request.urlopen(announce_request) as resp:
+			resp.read()
+		print(f'Announced transaction {transaction_hash[:16]}...')
+
+		# Wait for confirmation via WebSocket
+		async for raw_message in websocket:
+			message = json.loads(raw_message)
+			topic = message['topic']
+			message_hash = message['data']['meta']['hash']
+			name = topic.split('/')[0]
+			print(f'{name}: hash={message_hash[:16]}...')
+
+			if (name == 'confirmedAdded'
+					and message_hash == transaction_hash):
+				print(f'Transaction {transaction_hash[:16]}... confirmed')
+				break
+
+		# Unsubscribe before closing
+		for channel in channels:
+			await websocket.send(json.dumps({
+				'uid': uid,
+				'unsubscribe': channel
+			}))
+		print('Unsubscribed from all channels')
+
+
+try:
+	asyncio.run(main())
+except Exception as error:
+	print(error)
