1f190636def7392561e660f5941e455052a7a375
wayonb <wayon@symbol.dev>
2026-03-12 13:08:25 -0400

    [jenkins] fix: use sed to remove ^ and ~ instead

--- jenkins/shared-library/vars/configureArtifactRepository.groovy
+++ jenkins/shared-library/vars/configureArtifactRepository.groovy
@@ -10,6 +10,9 @@ void call(String environments, Boolean isPublicGitHubRepo) {
 			if ('javascript' == environment && fileExists('package-lock.json')) {
 				// remove package-lock.json file since the hashes will not match private repository
 				sh('rm -f package-lock.json')
+
+				// remove ^ and ~ from package.json to prevent npm from installing latest versions which can cause issues
+				sh('sed -i \'s/"[\\^~]/"/g\' package.json')
 			}
 		}
 	}
@@ -56,7 +59,6 @@ void configureNpm(Map info) {
 		npmrcContent += "\n//${hostNamePath}:_auth=${userNamePasswordEncoding}"
 	}
 
-	npmrcContent += '\n"config": {\n    "save-exact": true\n}'
 	writeFile(file: '.npmrc', text: npmrcContent)
 }
 
