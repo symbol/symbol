e1cb59fff5ec3a7b5a42e0c3befab43d940ba90f
zero <zero4862@pm.me>
2026-03-25 11:16:36 +0000

    [mkdocs] docs: add query currency supply tutorial (#1907)
    * [mkdocs] docs: add query currency supply tutorial
    
    * Update mkdocs/pages/en/devbook/network-currency/query-currency-supply.md
    
    Co-authored-by: Xavi Artigas <xavierartigas@yahoo.es>
    
    * [mkdocs] update to latest OpenAPI spec

---
+++ mkdocs/overrides/devbook/network-currency/query-currency-supply.py
@@ -0,0 +1,32 @@
+import os
+import urllib.request
+
+NODE_URL = os.getenv('NODE_URL', 'https://reference.symboltest.net:3001')
+print(f'Using node {NODE_URL}')
+
+SUPPLY_PATH = '/network/currency/supply'
+
+try:
+	with urllib.request.urlopen(
+			f'{NODE_URL}{SUPPLY_PATH}/max') as response:
+		maximum = float(response.read().decode().strip())
+	print(f'Maximum supply: {maximum:,.6f} XYM')
+
+	with urllib.request.urlopen(
+			f'{NODE_URL}{SUPPLY_PATH}/total') as response:
+		total = float(response.read().decode().strip())
+	print(f'Total supply: {total:,.6f} XYM')
+
+	with urllib.request.urlopen(
+			f'{NODE_URL}{SUPPLY_PATH}/circulating') as response:
+		circulating = float(response.read().decode().strip())
+	print(f'Circulating supply: {circulating:,.6f} XYM')
+
+	non_circulating = total - circulating
+	print(f'Non-circulating: {non_circulating:,.6f} XYM')
+
+	unminted = maximum - total
+	print(f'Unminted: {unminted:,.6f} XYM')
+
+except Exception as error:
+	print(error)
