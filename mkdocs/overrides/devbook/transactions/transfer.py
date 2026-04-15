symbol/symbol

commit 3964edc2c5256ef6417c8c779503b66f33e07d0d

[mkdocs] fix: get private key from environment in Transfer tutorial

mkdocs/overrides/devbook/transactions/transfer.py
@@ -12,8 +12,8 @@ from symbolchain.sc import Amount
 NODE_URL = 'https://reference.symboltest.net:3001'
 print(f'Using node {NODE_URL}')
 
-SIGNER_PRIVATE_KEY = (
-	'EDB671EB741BD676969D8A035271D1EE5E75DF33278083D877F23615EB839FEC')
+SIGNER_PRIVATE_KEY = os.getenv('SIGNER_PRIVATE_KEY',
+	'0000000000000000000000000000000000000000000000000000000000000000')
 signer_key_pair = SymbolFacade.KeyPair(PrivateKey(SIGNER_PRIVATE_KEY))
 
 facade = SymbolFacade('testnet')

mkdocs/overrides/devbook/transactions/transfer.mjs
@@ -9,8 +9,8 @@ import {
 const NODE_URL = 'https://reference.symboltest.net:3001';
 console.log('Using node', NODE_URL);
 
-const SIGNER_PRIVATE_KEY =
-	'EDB671EB741BD676969D8A035271D1EE5E75DF33278083D877F23615EB839FEC';
+const SIGNER_PRIVATE_KEY = process.env.SIGNER_PRIVATE_KEY ||
+	'0000000000000000000000000000000000000000000000000000000000000000';
 const signerKeyPair = new SymbolFacade.KeyPair(
 	new PrivateKey(SIGNER_PRIVATE_KEY));
 

commit 3eb1563718b543d5ad2e73dff0561731b8812385

[mkdocs] fix: missing import in Transfer Python tutorial
Which I broke in 3964edc2c5256ef6417c8c779503b66f33e07d0d

mkdocs/overrides/devbook/transactions/transfer.py
@@ -1,5 +1,6 @@
 import datetime
 import json
+import os
 import time
 import urllib.request
 