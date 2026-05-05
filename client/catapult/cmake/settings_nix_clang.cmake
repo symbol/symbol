759c56827b7fcb4aa58292b039305abc01024266
Alkoyd <alkoyd@symbolsyndicate.us>
2026-05-05 14:29:18 +0200

    Revert "Remove language gates for CLang"
    This reverts commit ac6a3b6fae3df41b4fdc8b7f000cb8aa63223d40.

--- client/catapult/cmake/settings_nix_clang.cmake
+++ client/catapult/cmake/settings_nix_clang.cmake
@@ -3,44 +3,53 @@ if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "14.0")
 	message(FATAL_ERROR "Clang version must be at least 14.0! Found version ${CMAKE_CXX_COMPILER_VERSION}")
 endif()
 
+# Set parallel build level for all generators (Make, Ninja, etc.)
+set(CMAKE_BUILD_PARALLEL_LEVEL ${PARALLEL_BUILDS})
+
+target_compile_definitions(build.defaults INTERFACE 
+	$<$<BOOL:${ARCHITECTURE_NAME}>:-march=${ARCHITECTURE_NAME}>
+)
 
 target_compile_options(build.defaults INTERFACE 
+	$<$<COMPILE_LANGUAGE:CXX>:-Weverything>										# enable all warnings
+	$<$<COMPILE_LANGUAGE:CXX>:-Werror>											# treat warnings as errors
+	$<$<COMPILE_LANGUAGE:CXX>:-Wno-c++98-compat>								# catapult is not compatible with C++98
+	$<$<COMPILE_LANGUAGE:CXX>:-Wno-c++98-compat-pedantic>						# catapult is not compatible with C++98
+	$<$<COMPILE_LANGUAGE:CXX>:-Wno-disabled-macro-expansion>					# expansion of recursive macro is required
+	$<$<COMPILE_LANGUAGE:CXX>:-Wno-padded>										# allow compiler to automatically pad data types for alignment
+	$<$<COMPILE_LANGUAGE:CXX>:-Wno-switch-enum>									# do not require enum switch statements to list every value
+	$<$<COMPILE_LANGUAGE:CXX>:-Wno-weak-vtables>								# vtables are emitted in all translation units for virtual classes with no out-of-line virtual method definitions
+	$<$<COMPILE_LANGUAGE:CXX>:-Wno-shadow-uncaptured-local>						# allow shadowing of local variables in lambdas https://github.com/llvm/llvm-project/issues/81307
 	$<$<BOOL:${ARCHITECTURE_NAME}>:-march=${ARCHITECTURE_NAME}>		
-	-Weverything									# enable all warnings
-	-Werror											# treat warnings as errors
-	-Wno-c++98-compat								# catapult is not compatible with C++98
-	-Wno-c++98-compat-pedantic						# catapult is not compatible with C++98
-	-Wno-disabled-macro-expansion					# expansion of recursive macro is required
-	-Wno-padded										# allow compiler to automatically pad data types for alignment
-	-Wno-switch-enum								# do not require enum switch statements to list every value
-	-Wno-weak-vtables								# vtables are emitted in all translation units for virtual classes with no out-of-line virtual method definitions
-	-Wno-shadow-uncaptured-local					# allow shadowing of local variables in lambdas https://github.com/llvm/llvm-project/issues/81307
-	$<$<VERSION_GREATER_EQUAL:${CMAKE_CXX_COMPILER_VERSION},16.0>:-Wno-unsafe-buffer-usage>			# allow unsafe buffer usage https://reviews.llvm.org/D137379
+	$<$<AND:$<COMPILE_LANGUAGE:CXX>,$<VERSION_GREATER_EQUAL:$<CXX_COMPILER_VERSION>,16.0>>:-Wno-unsafe-buffer-usage>      # allow unsafe buffer usage https://reviews.llvm.org/D137379
 
-	$<$<BOOL:${ENABLE_CODE_COVERAGE}>:-fprofile-instr-generate>
-	$<$<BOOL:${ENABLE_CODE_COVERAGE}>:-fcoverage-mapping>
+	$<$<AND:$<COMPILE_LANGUAGE:CXX>,$<BOOL:${ENABLE_CODE_COVERAGE}>>:-fprofile-instr-generate>
+	$<$<AND:$<COMPILE_LANGUAGE:CXX>,$<BOOL:${ENABLE_CODE_COVERAGE}>>:-fcoverage-mapping>
 
-	$<$<BOOL:${USE_SANITIZER}>:-fno-omit-frame-pointer>
-	$<$<BOOL:${USE_SANITIZER}>:-fsanitize=${USE_SANITIZER}>
-	$<$<BOOL:${USE_SANITIZER}>:-fsanitize-ignorelist=${PROJECT_SOURCE_DIR}/sanitizer_ignorelist.txt>
-	$<$<STREQUAL:${USE_SANITIZER},undefined>:-fsanitize=implicit-conversion,nullability>
-	$<$<AND:$<STREQUAL:${USE_SANITIZER},undefined>,$<BOOL:${ENABLE_FUZZ_BUILD}>>:-fsanitize=address>
-	$<$<AND:$<STREQUAL:${USE_SANITIZER},undefined>,$<BOOL:${ENABLE_FUZZ_BUILD}>>:-fno-sanitize-recover=all>
-	$<$<AND:$<STREQUAL:${USE_SANITIZER},undefined>,$<STREQUAL:${CMAKE_SYSTEM_NAME},Darwin>,$<STREQUAL:${CMAKE_SYSTEM_PROCESSOR},arm64>>:-fno-sanitize=vptr>
+	$<$<AND:$<COMPILE_LANGUAGE:CXX>,$<BOOL:${USE_SANITIZER}>>:-fno-omit-frame-pointer>
+	$<$<AND:$<COMPILE_LANGUAGE:CXX>,$<BOOL:${USE_SANITIZER}>>:-fsanitize=${USE_SANITIZER}>
+	$<$<AND:$<COMPILE_LANGUAGE:CXX>,$<BOOL:${USE_SANITIZER}>>:-fsanitize-ignorelist=${PROJECT_SOURCE_DIR}/sanitizer_ignorelist.txt>
+	$<$<AND:$<COMPILE_LANGUAGE:CXX>,$<STREQUAL:${USE_SANITIZER},undefined>>:-fsanitize=implicit-conversion,nullability>
+	$<$<AND:$<COMPILE_LANGUAGE:CXX>,$<STREQUAL:${USE_SANITIZER},undefined>,$<BOOL:${ENABLE_FUZZ_BUILD}>>:-fsanitize=address>
+	$<$<AND:$<COMPILE_LANGUAGE:CXX>,$<STREQUAL:${USE_SANITIZER},undefined>,$<BOOL:${ENABLE_FUZZ_BUILD}>>:-fno-sanitize-recover=all>
+	$<$<AND:$<COMPILE_LANGUAGE:CXX>,$<STREQUAL:${USE_SANITIZER},undefined>,$<STREQUAL:${CMAKE_SYSTEM_NAME},Darwin>,$<STREQUAL:${CMAKE_SYSTEM_PROCESSOR},arm64>>:-fno-sanitize=vptr>
 
-    $<$<OR:$<BOOL:${ENABLE_HARDENING}>,$<CONFIG:Release,RelWithDebInfo>>:-D_FORTIFY_SOURCE=3>
-	$<$<OR:$<BOOL:${ENABLE_HARDENING}>,$<CONFIG:Release,RelWithDebInfo>>:-fstack-protector-all>
-	$<$<OR:$<BOOL:${ENABLE_HARDENING}>,$<CONFIG:Release,RelWithDebInfo>>:-fsanitize=safe-stack>
+	$<$<AND:$<COMPILE_LANGUAGE:CXX>,$<OR:$<BOOL:${ENABLE_HARDENING}>,$<CONFIG:Release,RelWithDebInfo>>>:-D_FORTIFY_SOURCE=3>
+	$<$<AND:$<COMPILE_LANGUAGE:CXX>,$<OR:$<BOOL:${ENABLE_HARDENING}>,$<CONFIG:Release,RelWithDebInfo>>>:-fstack-protector-all>
+	$<$<AND:$<COMPILE_LANGUAGE:CXX>,$<OR:$<BOOL:${ENABLE_HARDENING}>,$<CONFIG:Release,RelWithDebInfo>>>:-fsanitize=safe-stack>
 
 	$<$<CONFIG:RelWithDebInfo>:-g1>
 )
 
-target_link_options(build.defaults INTERFACE
-	$<$<OR:$<BOOL:${ENABLE_HARDENING}>,$<CONFIG:Release,RelWithDebInfo>>:-fsanitize=safe-stack>
-	$<$<OR:$<BOOL:${ENABLE_HARDENING}>,$<CONFIG:Release,RelWithDebInfo>>:-Wl,-z,noexecstack>   # NX bit - prevent code execution from stack
-	$<$<OR:$<BOOL:${ENABLE_HARDENING}>,$<CONFIG:Release,RelWithDebInfo>>:-Wl,-z,relro>         # Read-only relocation - make reloc section read-only
-	$<$<OR:$<BOOL:${ENABLE_HARDENING}>,$<CONFIG:Release,RelWithDebInfo>>:-Wl,-z,now>           # Resolve all symbols immediately (no lazy binding)
-)
+if(ENABLE_HARDENING)
+	target_link_options(build.defaults INTERFACE
+		-fsanitize=safe-stack
+		-Wl,-z,noexecstack   # NX bit - prevent code execution from stack
+		-Wl,-z,relro         # Read-only relocation - make reloc section read-only
+		-Wl,-z,now           # Resolve all symbols immediately (no lazy binding)
+	)
+endif()
+
 
 # fix -Wpoison-system-directories: error: include location '/usr/local/include' is "unsafe for cross-compilation"
 if("${CMAKE_SYSTEM_NAME}" STREQUAL "Darwin" AND NOT DEFINED CMAKE_OSX_SYSROOT)
@@ -60,7 +69,7 @@ endif()
 target_compile_options(build.tests INTERFACE
 	# - Wno-global-constructors: required for GTEST test definition macros
 	# - Wno-zero-as-null-pointer-constant: workaround for GTEST NULL/nullptr mismatch https://github.com/google/googletest/issues/1323
-    $<$<BOOL:${ENABLE_TESTS}>:-Wno-global-constructors>
-	$<$<BOOL:${ENABLE_TESTS}>:-Wno-zero-as-null-pointer-constant>
-	$<$<BOOL:${ENABLE_TESTS}>:-Wno-missing-noreturn>
+    $<$<AND:$<BOOL:${ENABLE_TESTS}>,$<COMPILE_LANGUAGE:CXX>>:-Wno-global-constructors>
+    $<$<AND:$<BOOL:${ENABLE_TESTS}>,$<COMPILE_LANGUAGE:CXX>>:-Wno-zero-as-null-pointer-constant>
+    $<$<AND:$<BOOL:${ENABLE_TESTS}>,$<COMPILE_LANGUAGE:CXX>>:-Wno-missing-noreturn>
 )
