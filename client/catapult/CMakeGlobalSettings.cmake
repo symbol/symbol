44af3ecee99274e3630c49f16d394fecc2ab4cae
Alkoyd <alkoyd@symbolsyndicate.us>
2026-05-09 21:44:43 +0200

    Rework custom functions [WIP]

--- client/catapult/CMakeGlobalSettings.cmake
+++ client/catapult/CMakeGlobalSettings.cmake
@@ -1,21 +1,4 @@
 
-### define version helpers
-if(MSVC)
-	function(set_win_version_definitions TARGET_NAME FILETYPE)
-		target_compile_definitions(${TARGET_NAME} PUBLIC
-			CATAPULT_VERSION_DESCRIPTION="${CATAPULT_VERSION_DESCRIPTION}"
-			WIN_FILETYPE=${FILETYPE}
-			$<$<BOOL:${CATAPULT_BUILD_RELEASE}>:CATAPULT_BUILD_RELEASE=1>
-		)
-	endfunction()
-
-	# embed the version rc file
-	set(VERSION_RESOURCES ${CMAKE_SOURCE_DIR}/src/catapult/version/win/win_version.rc)
-else()
-	# since strings are not referenced, in order for the linker to include them, they must be forcibly linked via an object file
-	set(VERSION_RESOURCES $<TARGET_OBJECTS:catapult.version.nix>)
-endif()
-
 ### define target helper functions
 
 # used to define a catapult target (library, executable) and automatically enables PCH for clang
@@ -31,36 +14,17 @@ endfunction()
 
 # combines catapult_library and catapult_target
 function(catapult_library_target TARGET_NAME)
-	add_library(${TARGET_NAME})
-	add_target_sources(${TARGET_NAME} DIRS ${ARGN})
-	catapult_target(${TARGET_NAME})
+	add_target(${TARGET_NAME} TYPE LIBRARY SOURCE_DIRS ${ARGN})
 endfunction()
 
 # used to define a catapult shared library target, creating an appropriate source group and adding a library
 function(catapult_shared_library_target TARGET_NAME)
-	add_library(${TARGET_NAME} SHARED)
-	add_target_sources(${TARGET_NAME} DIRS ${ARGN})
-	target_sources(${TARGET_NAME} PRIVATE ${VERSION_RESOURCES})
-	if(MSVC)
-		set_win_version_definitions(${TARGET_NAME} VFT_DLL)
-	endif()
-
-	catapult_target(${TARGET_NAME})
-	install(TARGETS ${TARGET_NAME})
+	add_target(${TARGET_NAME} TYPE LIBRARY LINKAGE SHARED SOURCE_DIRS ${ARGN})
 endfunction()
 
 # used to define a catapult executable, creating an appropriate source group and adding an executable
 function(catapult_executable TARGET_NAME)
-	add_executable(${TARGET_NAME} ${VERSION_RESOURCES})
-	add_target_sources(${TARGET_NAME} DIRS ${ARGN})
-
-	if(MSVC)
-		set_win_version_definitions(${TARGET_NAME} VFT_APP)
-	endif()
-
-	if(WIN32 AND MINGW)
-		target_link_libraries(${TARGET_NAME} wsock32 ws2_32)
-	endif()
+	add_target(${TARGET_NAME} TYPE EXECUTABLE SOURCE_DIRS ${ARGN})
 endfunction()
 
 # used to define a catapult header only target, creating an appropriate source group in order to allow VS to create an appropriate folder
