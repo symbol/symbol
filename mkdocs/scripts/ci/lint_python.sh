7ef57e81f3af5f2d2a8a5bfc1346c40298feab15
Xavi Artigas <xavi@symbolsyndicate.us>
2026-05-11 19:14:44 +0200

    [mkdocs] fix: pylint linting fixes

--- mkdocs/scripts/ci/lint_python.sh
+++ mkdocs/scripts/ci/lint_python.sh
@@ -10,5 +10,13 @@ find snippets \( -name node_modules -o -name .venv \) -prune -o -type f -name "*
 	--check-only
 find snippets \( -name node_modules -o -name .venv \) -prune -o -type f -name "*.py" -print0 | PYTHONPATH=. xargs -0 python3 -m pycodestyle \
 	--config=.pycodestyle
+
+# Build a custom .pylintrc file based on the global one
+TMP_RC_FILE=/tmp/symbol-docs.pylintrc
+cp "$(git rev-parse --show-toplevel)/linters/python/.pylintrc" $TMP_RC_FILE
+# Allow lowercase "constants" (actually, top-level regular variables)
+echo "const-rgx=(([A-Za-z_][A-Za-z0-9_]*)|(t_[A-Z0-9_]+)|(__.*__))$" >> $TMP_RC_FILE
+# Disable some warnings we accept for tutorial code
+echo "disable=missing-docstring,broad-exception-caught,duplicate-code,use-maxsplit-arg,too-many-locals,too-many-branches,too-many-statements" >> $TMP_RC_FILE
 find snippets \( -name node_modules -o -name .venv \) -prune -o -type f -name "*.py" -print0 | PYTHONPATH=. xargs -0 python3 -m pylint \
-	--rcfile "$(git rev-parse --show-toplevel)/linters/python/.pylintrc"
+	--rcfile $TMP_RC_FILE
