1d2b5a92c2f3f05ecd4a54eb398fb45994bf9cf2
Xavi Artigas <xavi@symbolsyndicate.us>
2026-05-15 12:17:38 +0200

    [mkdocs] fix: do not use abbreviations in query-currency-supply

--- mkdocs/snippets/devbook/network-currency/query_currency_supply.py
+++ mkdocs/snippets/devbook/network-currency/query_currency_supply.py
@@ -8,22 +8,22 @@ SUPPLY_URL = f'{NODE_URL}/network/currency/supply'
 
 try:
 	with urllib.request.urlopen(f'{SUPPLY_URL}/max') as response:  # [>step-1]
-		maximum = float(response.read().decode().strip())
-	print(f'Maximum supply: {maximum:,.6f} XYM')
+		maximum_supply = float(response.read().decode().strip())
+	print(f'Maximum supply: {maximum_supply:,.6f} XYM')
 
 	with urllib.request.urlopen(f'{SUPPLY_URL}/total') as response:
-		total = float(response.read().decode().strip())
-	print(f'Total supply: {total:,.6f} XYM')
+		total_supply = float(response.read().decode().strip())
+	print(f'Total supply: {total_supply:,.6f} XYM')
 
 	with urllib.request.urlopen(f'{SUPPLY_URL}/circulating') as response:
-		circulating = float(response.read().decode().strip())
-	print(f'Circulating supply: {circulating:,.6f} XYM')  # [<step-1]
+		circulating_supply = float(response.read().decode().strip())
+	print(f'Circulating supply: {circulating_supply:,.6f} XYM')  # [<step-1]
 	# [>step-2]
-	non_circulating = total - circulating
-	print(f'Non-circulating: {non_circulating:,.6f} XYM')
+	non_circulating_supply = total_supply - circulating_supply
+	print(f'Non-circulating supply: {non_circulating_supply:,.6f} XYM')
 
-	unminted = maximum - total
-	print(f'Unminted: {unminted:,.6f} XYM')
+	unminted_supply = maximum_supply - total_supply
+	print(f'Unminted supply: {unminted_supply:,.6f} XYM')
 	# [<step-2]
 except Exception as error:
 	print(error)
