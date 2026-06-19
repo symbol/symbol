6167856a1cbdea8269081c433142d306751775cd
wayonb <wayon@symbolsyndicate.us>
2026-06-18 22:15:27 -0400

    [jenkins]: fix linting

--- jenkins/shared-library/vars/defaultCiPipeline.groovy
+++ jenkins/shared-library/vars/defaultCiPipeline.groovy
@@ -385,7 +385,7 @@ List<String> resolveCiEnvironment(Map params) {
 	List<String> environmentTags = params.otherEnvironments?.clone() ?: []
 
 	// default environment is LTS
-	String defaultEnvironment = environmentName.contains("-") ? environmentName : "${environmentName}-${params.operatingSystem[0]}-lts"
+	String defaultEnvironment = environmentName.contains('-') ? environmentName : "${environmentName}-${params.operatingSystem[0]}-lts"
 	environmentTags.add(0, defaultEnvironment)
 	return environmentTags
 }
