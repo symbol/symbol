a852b7708619ad65544d563ea14ad8c7c1b9b42c
Xavi Artigas <xavi@symbolsyndicate.us>
2026-06-26 13:59:22 +0200

    [mkdocs] fix: gh-pages deployment scripts

--- mkdocs/scripts/deploy-gh-pages.sh
+++ mkdocs/scripts/deploy-gh-pages.sh
@@ -2,17 +2,16 @@
 
 set -ex
 
+rm -rf ../docs ../docs-staging
 SYMBOL_DOCS_DISABLE_TS=false mkdocs build -f config/mkdocs.en.yml
 SYMBOL_DOCS_DISABLE_TS=false mkdocs build -f config/mkdocs.ja.yml
-cd ../docs
-mv en en2
-mv ja ja2
+cd ..
+mv docs docs-staging
 git checkout gh-pages
-rm -rf en ja
-mv en2 en
-mv ja2 ja
-git add -f en ja
+rm -rf docs
+mv docs-staging docs
+git add -f docs
 git commit -m "[docs] Update"
 git push
 git checkout new-docs
-cd ../mkdocs
+cd mkdocs
