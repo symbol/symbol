9f205f3b6bce478c761f0de9198caf5863401c36
Xavi Artigas <xavi@symbolsyndicate.us>
2026-06-26 16:36:21 +0200

    [mkdocs] fix: align comments

--- mkdocs/scripts/register_cats_lexer.py
+++ mkdocs/scripts/register_cats_lexer.py
@@ -1,9 +1,9 @@
 from pygments.lexers._mapping import LEXERS
 
 LEXERS['CATSLexer'] = (
-	'cats_lexer.cats_lexer',    # module name
-	'CATS',           # display name
-	('cats',),        # aliases
-	('*.cats',),      # file extensions
-	('text/x-cats',)  # mimetypes
+	'cats_lexer.cats_lexer', # module name
+	'CATS',                  # display name
+	('cats',),               # aliases
+	('*.cats',),             # file extensions
+	('text/x-cats',)         # mimetypes
 )
