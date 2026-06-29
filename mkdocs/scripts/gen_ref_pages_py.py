3460640fc125b87191ffd1dc58421fe171b70d9f
Xavi Artigas <xavi@symbolsyndicate.us>
2026-06-29 13:48:25 +0200

    [mkdocs] fix: lint build scripts WIP

--- mkdocs/scripts/gen_ref_pages_py.py
+++ mkdocs/scripts/gen_ref_pages_py.py
@@ -7,17 +7,17 @@ ignored_files = mkdocs_gen_files.config['extra']['symbol']['py-sdk']['ignore-fil
 ignored_folders = mkdocs_gen_files.config['extra']['symbol']['py-sdk']['ignore-folders']
 
 root = Path(__file__).parent.parent.parent
-src = root / "sdk/python/symbolchain"
-paths = sorted(src.rglob("*.py"))
+src = root / 'sdk/python/symbolchain'
+paths = sorted(src.rglob('*.py'))
 
 for path in paths:
-	module_path = path.relative_to(src.parent).with_suffix("")
-	doc_path = path.relative_to(src).with_suffix(".md")
-	full_doc_path = Path("devbook/reference/py", doc_path)
+	module_path = path.relative_to(src.parent).with_suffix('')
+	doc_path = path.relative_to(src).with_suffix('.md')
+	full_doc_path = Path('devbook/reference/py', doc_path)
 
 	parts = tuple(module_path.parts)
 
-	if parts[-1] == "__init__":
+	if parts[-1] == '__init__':
 		parts = parts[:-1]
 	if parts[-1] in ignored_files:
 		continue
@@ -26,13 +26,13 @@ for path in paths:
 
 	nav[parts] = doc_path.as_posix()
 
-	with mkdocs_gen_files.open(full_doc_path, "w") as fd:
-		identifier = ".".join(parts)
+	with mkdocs_gen_files.open(full_doc_path, 'w') as fd:
+		identifier = '.'.join(parts)
 		print(f'# :simple-python: {parts[-1]}', file=fd)
 		print('', file=fd)
-		print("::: " + identifier, file=fd)
+		print('::: ' + identifier, file=fd)
 
-with mkdocs_gen_files.open("devbook/reference/py/links.md", "w") as nav_file:
+with mkdocs_gen_files.open('devbook/reference/py/links.md', 'w') as nav_file:
 	# Exclude the index file from being indexed by the search plugin
-	nav_file.writelines(["---\n", "search:\n", "    exclude: true\n", "---\n\n"])
+	nav_file.writelines(['---\n', 'search:\n', '    exclude: true\n', '---\n\n'])
 	nav_file.writelines(nav.build_literate_nav())
--- mkdocs/scripts/gen_ref_pages_ts.py
+++ mkdocs/scripts/gen_ref_pages_ts.py
@@ -6,22 +6,22 @@ nav = mkdocs_gen_files.Nav()
 
 # Generate index file for the TypeScript API ref
 for f in mkdocs_gen_files.editor.FilesEditor.current().files:
-	if not f.src_uri.startswith("devbook/reference/ts/"):
+	if not f.src_uri.startswith('devbook/reference/ts/'):
 		continue
-	path = Path(f.src_uri.removeprefix("devbook/reference/ts/"))
-	if path.stem == "README" or path.stem == ".meta":
+	path = Path(f.src_uri.removeprefix('devbook/reference/ts/'))
+	if path.stem in ('README', '.meta'):
 		continue
-	if "nem" in path.parts:
+	if 'nem' in path.parts:
 		continue
 
-	module_path = path.relative_to(".").with_suffix("")
+	module_path = path.relative_to('.').with_suffix('')
 
-	p = [i for i in module_path.parts if i not in ["namespaces", "classes", "functions"]]
+	p = [i for i in module_path.parts if i not in ['namespaces', 'classes', 'functions']]
 	parts = tuple(p)
 
 	nav[parts] = path.as_posix()
 
-with mkdocs_gen_files.open("devbook/reference/ts/links.md", "w") as nav_file:
+with mkdocs_gen_files.open('devbook/reference/ts/links.md', 'w') as nav_file:
 	# Exclude the index file from being indexed by the search plugin
-	nav_file.writelines(["---\n", "search:\n", "    exclude: true\n", "---\n\n"])
+	nav_file.writelines(['---\n', 'search:\n', '    exclude: true\n', '---\n\n'])
 	nav_file.writelines(nav.build_literate_nav())
