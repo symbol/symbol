2df151464cd522e58f5feab694541cb39b858c39
Alkoyd <alkoyd@symbolsyndicate.us>
2026-07-02 16:02:04 +0200

    refactor: drop duplicate init and redundant access specifier in SocketGuard

--- client/catapult/src/catapult/ionet/PacketSocket.cpp
+++ client/catapult/src/catapult/ionet/PacketSocket.cpp
@@ -84,8 +84,7 @@ namespace catapult { namespace ionet {
 					: m_strand(boost::asio::make_strand(ioContext))
 					, m_strandWrapper(m_strand)
 					, m_socket(ioContext, sslContext)
-					, m_drainTimer(ioContext)
-					, m_isFinalizing(false) {
+					, m_drainTimer(ioContext) {
 				m_isClosed.clear();
 				m_isClosed.test_and_set();
 			}
@@ -94,8 +93,7 @@ namespace catapult { namespace ionet {
 					: m_strand(boost::asio::make_strand(ioContext))
 					, m_strandWrapper(m_strand)
 					, m_socket(std::move(socket), sslContext)
-					, m_drainTimer(ioContext)
-					, m_isFinalizing(false) {
+					, m_drainTimer(ioContext) {
 				m_isClosed.clear();
 				m_isClosed.test_and_set();
 			}
@@ -214,13 +212,12 @@ namespace catapult { namespace ionet {
 		private:
 			static constexpr auto Drain_Timeout = std::chrono::milliseconds(100);
 
-		private:
 			Strand m_strand;
 			thread::StrandOwnerLifetimeExtender<SocketGuard> m_strandWrapper;
 			Socket m_socket;
 			boost::asio::steady_timer m_drainTimer;
 			std::array<uint8_t, 1024> m_drainBuffer;
-			bool m_isFinalizing;
+			bool m_isFinalizing = false;
 			std::atomic_flag m_isClosed;
 		};
 
