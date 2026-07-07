ab89da7352007633fba369453bfeaf9892349b17
wayonb <wayon@symbolsyndicate.us>
2026-07-06 21:56:45 -0400

    [linters]: add docs to the gitlint category
    problem: gitlint is failing due to no docs category
    solution: add a docs to gitlint category

--- linters/scripts/lint_last_commit.sh
+++ linters/scripts/lint_last_commit.sh
@@ -4,7 +4,7 @@ set -ex
 
 GITLINT_COMMON_MODULES="dependency|jenkins|monorepo"
 GITLINT_MODULES="${GITLINT_COMMON_MODULES}|$(cat "$(git rev-parse --show-toplevel)"/.gitlintmodules)"
-GITLINT_CATEGORIES="feat|bug|fix|build|perf|task"
+GITLINT_CATEGORIES="bug|build|docs|feat|fix|perf|task"
 gitlint \
 	-C "$(git rev-parse --show-toplevel)/linters/git/.gitlint" \
 	-c title-match-regex.regex="^\\[(${GITLINT_MODULES})\\]( (${GITLINT_CATEGORIES}))?: [\S ]+[a-zA-Z0-9]( \(#[0-9]+\))?$"
